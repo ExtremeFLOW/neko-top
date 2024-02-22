ffdf2a022217bd5c6ac6a5fd5b3fe1ca0a63cf71
Tim Felle Olsen <tife@dtu.dk>
2024-02-22 14:46:18 +0100

    Include the prepare script when running examples

--- run.sh
+++ run.sh
@@ -48,6 +48,9 @@ export SPATH="$MAIN_DIR/scripts/"                 # Scripts folder
 export HPATH="$MAIN_DIR/scripts/jobscripts/LSF10" # Submission settings
 export DPATH="$MAIN_DIR/data"                     # Meshes
 
+if [ -f "$MAIN_DIR/prepare.sh" ]; then
+    source $MAIN_DIR/prepare.sh
+fi
 [ -z "$NEKO_DIR" ] && export NEKO_DIR="$MAIN_DIR/external/neko"
 
 # End of user inputs
