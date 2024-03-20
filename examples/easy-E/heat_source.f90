5ab6dfb444b9ea66cbe4ca43770b667209c16b03
Tim Felle Olsen <tife@dtu.dk>
2024-03-20 09:58:45 +0100

    Update to easy-E

--- examples/easy-E/heat_source.f90
+++ examples/easy-E/heat_source.f90
@@ -14,7 +14,9 @@ module user
   real(kind=rp), dimension(:), allocatable :: resistance
   type(c_ptr) :: resistance_d = c_null_ptr
   logical :: is_initialized = .false.
-  real(kind=rp) :: Pe
+  real(kind=rp) :: Pe, target_temperature
+
+  real(kind=rp) :: ramp_time_end = 0.0_rp
 
 contains
 
@@ -36,6 +38,9 @@ contains
 
     call json_get(params, "case.fluid.Re", Re)
     call json_get(params, "case.scalar.Pe", Pe)
+    call json_get(params, "case.scalar.target_temperature", target_temperature)
+    call json_get(params, "case.end_time", ramp_time_end)
+    ramp_time_end = ramp_time_end * 0.01_rp
 
     mu = 1.0_rp/Re
     lambda = 1.0_rp/Pe
@@ -43,47 +48,20 @@ contains
     cp = 1.0_rp
   end subroutine set_material_properties
 
-  !> Set the initial condition for the scalar field
-  !! @details This function will initialize the scalar field with a two part
-  !! uniform value. Above z=0 the scalar field will be 0.0 and below z=0 the
-  !! scalar field will be 1.0.
-  !!
-  !! @param[inout] s Scalar field
-  !! @param[inout] params JSON file
-  subroutine initial_condition(s, params)
-    type(field_t), intent(inout) :: s
-    type(json_file), intent(inout) :: params
-
-    real(kind=rp) :: z_value
-    integer :: i
-
-    do i = 1, s%dof%size()
-       z_value = s%dof%z(i, 1, 1, 1)
-
-       if (z_value > 0.0_rp) then
-          s%x(i, 1, 1, 1) = 0.0_rp
-       else
-          s%x(i, 1, 1, 1) = 1.0_rp
-       end if
-
-    end do
-
-  end subroutine initial_condition
-
   !> Heat source
   subroutine heat_source(rhs, t)
-    use math_ext, only: sub3_mask
-    use device_math_ext, only: device_sub3_mask
+    use math_ext, only: sub3_mask, cfill_mask
+    use device_math_ext, only: device_sub3_mask, device_cfill_mask
 
     class(scalar_user_source_term_t), intent(inout) :: rhs
     real(kind=rp), intent(in) :: t
-    type(field_t), pointer :: u, v, w, s
+    type(field_t), pointer :: s
 
     integer :: i
-    real(kind=rp) :: target_temp
     class(point_zone_t), pointer :: cyl, ball
 
-    target_temp = 100.0_rp
+    real(kind=rp) :: current_temperature
+
     cyl => neko_point_zone_registry%get_point_zone("cylinder")
     ball => neko_point_zone_registry%get_point_zone("ball")
 
@@ -94,12 +72,12 @@ contains
 
        ! Set the resistance to 1.0 in the cylinder
        do i = 1, cyl%size
-          resistance(cyl%mask(i)) = target_temp
+          resistance(cyl%mask(i)) = target_temperature
        end do
 
        ! Set the resistance to 1.0 in the ball
        do i = 1, ball%size
-          resistance(ball%mask(i)) = target_temp
+          resistance(ball%mask(i)) = target_temperature
        end do
 
        ! Copy to device
@@ -112,14 +90,24 @@ contains
 
     s => neko_field_registry%get_field('s')
 
+    current_temperature = min(1.0_rp, t / ramp_time_end) * target_temperature
+
     if (NEKO_BCKND_DEVICE .eq. 1) then
-       call device_cfill(rhs%s_d, 0.0_rp, rhs%dm%size())
-       call device_sub3_mask(rhs%s_d, resistance_d, s%x_d, rhs%dm%size(), cyl%mask_d, cyl%size)
-       call device_sub3_mask(rhs%s_d, resistance_d, s%x_d, rhs%dm%size(), ball%mask_d, ball%size)
+       call device_cfill_mask(s%x_d, current_temperature, rhs%dm%size(), cyl%mask_d, cyl%size)
+       call device_cfill_mask(s%x_d, current_temperature, rhs%dm%size(), ball%mask_d, ball%size)
+
+
+       !  call device_cfill(rhs%s_d, 0.0_rp, rhs%dm%size())
+       !  call device_sub3_mask(rhs%s_d, resistance_d, s%x_d, rhs%dm%size(), cyl%mask_d, cyl%size)
+       !  call device_sub3_mask(rhs%s_d, resistance_d, s%x_d, rhs%dm%size(), ball%mask_d, ball%size)
     else
-       call cfill(rhs%s, 0.0_rp, rhs%dm%size())
-       call sub3_mask(rhs%s, resistance, s%x, rhs%dm%size(), cyl%mask, cyl%size)
-       call sub3_mask(rhs%s, resistance, s%x, rhs%dm%size(), ball%mask, ball%size)
+
+       call cfill_mask(s%x, current_temperature, rhs%dm%size(), cyl%mask, cyl%size)
+       call cfill_mask(s%x, current_temperature, rhs%dm%size(), ball%mask, ball%size)
+
+       !  call cfill(rhs%s, 0.0_rp, rhs%dm%size())
+       !  call sub3_mask(rhs%s, resistance, s%x, rhs%dm%size(), cyl%mask, cyl%size)
+       !  call sub3_mask(rhs%s, resistance, s%x, rhs%dm%size(), ball%mask, ball%size)
     end if
 
   end subroutine heat_source
