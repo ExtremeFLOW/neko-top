d7348f563da8e89155cab8eeac2747439f0e3aad
Tim Felle Olsen <tife@dtu.dk>
2024-03-27 08:38:26 +0100

    Update the setup script

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -29,7 +29,11 @@ function check_system_dependencies() {
 function find_json_fortran() {
 
     # Ensure JSON-Fortran is installed, if not install it.
-    if [[ -z "$(find $1 -name libjsonfortran.so)" && -f $1/CMakeLists.txt ]]; then
+    if [[ -z "$(find $1 -name libjsonfortran.so)" ]]; then
+        if [ ! -f "$1/CMakeLists.txt" ]; then
+            git clone https://github.com/jacobwilliams/json-fortran $1
+        fi
+
         cmake -S $1 -B $1/build \
             --install-prefix $1 \
             -Wno-dev \
@@ -41,7 +45,8 @@ function find_json_fortran() {
         rm -fr $1/build
     fi
 
-    JSON_FORTRAN=$(find $1 -type d -exec test -f '{}'/libjsonfortran.so \; -print)
+    JSON_FORTRAN=$(find $1 -type d \
+        -exec test -f '{}'/libjsonfortran.so \; -print)
     if [ -z "$JSON_FORTRAN" ]; then
         error "JSON-Fortran not found at:"
         error "\t$1"
@@ -58,7 +63,7 @@ function find_json_fortran() {
 
     # Setup environment variables
     export PKG_CONFIG_PATH="$JSON_FORTRAN/pkgconfig:$PKG_CONFIG_PATH"
-    export LD_LIBRARY_PATH="$JSON_FORTRAN):$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="$JSON_FORTRAN:$LD_LIBRARY_PATH"
 }
 
 # ============================================================================ #
@@ -73,8 +78,20 @@ function find_gslib() {
         cd $current
     fi
 
-    GSLIB="$(find $1 -type d -exec test -f '{}/lib/libgs.a' \; -print)"
-    export GSLIB=$(realpath $GSLIB)
+    GSLIB_DIR="$(find $1 -type d -exec test -f '{}/lib/libgs.a' \; -print)"
+    if [ -z "$GSLIB_DIR" ]; then
+        error "GSLIB not found at:"
+        error "\t$1"
+        error "Please set GSLIB_DIR to the directory containing"
+        error "the GSLIB source code."
+        error "You can download the source code from:"
+        error "\thttps://github.com/Nek5000/gslib"
+        error "Or invoke the git submodule command:"
+        error "\tgit submodule update --init --recursive"
+        exit 1
+    fi
+
+    export GSLIB_DIR=$(realpath $GSLIB_DIR)
 }
 
 # ============================================================================ #
@@ -84,11 +101,10 @@ function find_pfunit() {
 
     if [ ! $TEST ]; then return; fi
 
-    if [[ -z "$(find $1 -name libpfunit.a)" && -f $1/CMakeLists.txt ]]; then
+    if [[ -z "$(find $1 -name libpfunit.a)" ]]; then
 
         if [ ! -f "$1/CMakeLists.txt" ]; then
-            printf "Installing PFUnit\n"
-            git submodule update --init external/pFUnit
+            git clone https://github.com/Goddard-Fortran-Ecosystem/pFUnit.git $1
         fi
 
         cmake -B $1/build -S $1 -G "Unix Makefiles" \
@@ -99,12 +115,12 @@ function find_pfunit() {
 
     PFUNIT_DIR=$(find $1 -type d -exec test -f '{}'/lib/libpfunit.a \; -print)
     if [ -z "$PFUNIT_DIR" ]; then
-        error "JSON-Fortran not found at:"
+        error "pFUnit not found at:"
         error "\t$1"
-        error "Please set JSON_FORTRAN_DIR to the directory containing"
-        error "the JSON-Fortran source code."
+        error "Please set PFUNIT_DIR to the directory containing"
+        error "the pFUnit source code."
         error "You can download the source code from:"
-        error "\thttps://github.com/jacobwilliams/json-fortran"
+        error "\thttps://github.com/Goddard-Fortran-Ecosystem/pFUnit.git"
         error "Or invoke the git submodule command:"
         error "\tgit submodule update --init --recursive"
         exit 1
