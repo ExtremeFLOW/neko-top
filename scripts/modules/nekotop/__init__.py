a778a39af0698bd794bfbc1f4eb2c38134ecc7e2
Tim Felle Olsen <tife@dtu.dk>
2024-04-03 15:10:01 +0200

    Starting to do python experiments

---
+++ scripts/modules/nekotop/__init__.py
@@ -0,0 +1,1 @@
+from .read_probes import read_probes
---
+++ scripts/modules/nekotop/read_probes.py
@@ -0,0 +1,46 @@
+import csv
+import os
+import numpy as np
+
+
+def read_probes(file_name) -> tuple:
+    """
+    Read in the probes file and return the points, fields, times and field
+    names.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to read in.
+
+    Returns
+    -------
+    points : np.array
+        The points in the domain.
+    fields : np.array
+        The fields at each time step.
+    times : np.array
+        The times at each time step.
+    field_names : list
+        The names of the fields.
+    """
+
+    with open(file_name) as f:
+        N_lines = sum(1 for _ in f)
+
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f)
+
+        # Read in the header
+        header = next(reader)
+        N_points = int(header[0])
+        field_names = header[2:]
+
+        N_times = (N_lines - 1 - N_points)
+
+        points = np.array([next(reader) for _ in range(N_points)], dtype=float)
+        fields = np.array([next(reader) for _ in range(N_times)], dtype=float)
+        times = fields[:, 0]
+        fields = fields[:, 1:]
+
+    return points, fields, times, field_names
