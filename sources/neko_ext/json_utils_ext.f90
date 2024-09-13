c025c5de5b087ff95e70f959349ab635605399fa
Tim Felle Olsen <tife@dtu.dk>
2024-09-13 11:08:31 +0200

    Add extended functionality for json
    JSON fallback for keys.

---
+++ sources/neko_ext/json_utils_ext.f90
@@ -0,0 +1,24 @@
+module json_utils_ext
+  use json_module, only: json_file
+  implicit none
+  private
+
+  public :: json_key_fallback
+
+contains
+
+  !> Create a json_string based on fallback logic.
+  function json_key_fallback(json, lookup, root, fallback) result(string)
+    type(json_file), intent(inout) :: json
+    character(len=*), intent(in) :: lookup
+    character(len=*), intent(in) :: root
+    character(len=*), intent(in) :: fallback
+    character(len=:), allocatable :: string
+
+    string = trim(adjustl(root)) // '.' // trim(adjustl(lookup))
+    if (.not. json%valid_path(string)) then
+       string = trim(adjustl(fallback)) // '.' // trim(adjustl(lookup))
+    end if
+  end function json_key_fallback
+
+end module json_utils_ext
