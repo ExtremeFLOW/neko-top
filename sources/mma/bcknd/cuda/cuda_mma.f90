f7d04a26b4d3ebc041d3d7cb7e3cf9a9b13abd1a
sysulp <sysulp@hotmail.com>
2024-10-03 14:08:22 +0200

    update

--- sources/mma/bcknd/cuda/cuda_mma.f90
+++ sources/mma/bcknd/cuda/cuda_mma.f90
@@ -4,11 +4,13 @@ module cuda_mma
   public
 
   interface
-     subroutine mma_gensub_gpu(void* x, void*  xold1, void* xold2, void* df0dx, void* dfdx, void* xlow, void* xupp, &
-	 void *xmin, void* xmax, void* alpha, void* beta, void* p0j, void* q0j, void* pij, void* qij, void *bi,
-	 real *asyinit, real* asydecr, real* asyincr, int* n, int* m, int* iter) &
+     subroutine mma_gensub_gpu( x_d,  xold1_d, xold2_d, df0dx_d, dfdx_d, xlow_d, xupp_d, xmin_d, xmax_d, alpha_d, beta_d, p0j_d, q0j_d, pij_d, qij_d, bi_d,&
+	 asyinit, asydecr, asyincr, n, m, iter) &
           bind(c, name = 'mma_gensub_gpu')
-
+		use, intrinsic :: iso_c_binding, only: c_int, c_ptr
+		type(c_ptr), value :: x_d,  xold1_d, xold2_d, df0dx_d, dfdx_d, xlow_d, xupp_d, xmin_d, xmax_d, alpha_d, beta_d, p0j_d, q0j_d, pij_d, qij_d, bi_d
+		real(c_rp) :: asyinit, asydecr, asyincr
+		integer(c_int) :: n, m, iter
      end subroutine mma_gensub_gpu
   end interface
 end module cuda_mma
