81694660b129c6f89a83a8e8e1fddff59a53ac0b
sysulp <sysulp@hotmail.com>
2024-10-04 11:15:12 +0200

    1

--- sources/mma/bcknd/cuda/mma_gpu.cu
+++ sources/mma/bcknd/cuda/mma_gpu.cu
@@ -3,34 +3,46 @@
 #include <mma_gpu_kernel.h>
 #include <stdio.h>
 #include <stdlib.h>
-void mma_gensub_gpu(void* x, void* xold1, void* xold2, void* df0dx, void* dfdx, void* xlow, void* xupp, void* xmin, void* xmax, void* alpha, void* beta, void* p0j, void* q0j, void* pij, void* qij, void* bi,
-	real* asyinit, real* asydecr, real* asyincr, int* n, int* m, int* iter) {
-	int num1 = *n;
-	int num2 = *m;
-	const dim3 nthrds(1024, 1, 1);
-	const dim3 nblcks((num1 + 1024 - 1) / 1024, 1, 1);
-	real* temp;
-	real* temp_sum;
-	cudaMalloc(&temp, num1 * num2 * sizeof(real));
-	cudaMalloc(&temp_sum, num1 * sizeof(real));
+void mma_gensub_gpu(void* x, void* xold1, void* xold2, void* df0dx, void* dfdx, void* xlow, void* xupp, void* xmin, void* xmax,
+    void* alpha, void* beta, void* p0j, void* q0j, void* pij, void* qij, void* bi,
+    real* asyinit, real* asydecr, real* asyincr, int* n, int* m, int* iter) {
+    int num1 = *n;
+    int num2 = *m;
 
-	for (int i = 0; i < num2; i++) {
-		int nb = (num1 + 2048 - 1) / 2048;
-		mmasum_kernel <real><< <nb, 1024 >> > (temp, temp_sum, num1, i);
+    const dim3 nthrds(1024, 1, 1);
+    const dim3 nblcks((num1 + 1024 - 1) / 1024, 1, 1);
+    real* temp;
+    real* temp_sum;
+    cudaMalloc(&temp, num1 * num2 * sizeof(real));
+    cudaMalloc(&temp_sum, num1 * sizeof(real));
 
-		mmareduce_kernel<real> << <1, 1024, 0 >> > (temp_sum, nb);
 
-		cudaMemcpy(bi + num2, temp_sum, sizeof(real), cudaMemcpyDeviceToDevice);
-	}
-	cudaFree(temp);
-	cudaFree(temp_sum);
-	////global communication ?
-	/*
-	globaltmp_m = 0.0_rp
-	call MPI_Allreduce(this%bi%x, globaltmp_m, this%m, &
-		 mpi_real_precision, mpi_sum, neko_comm, ierr)
-	this%bi%x = globaltmp_m - fval
-	*/
+    BoundCalculation_kernel<real> << <nblcks, nthrds, 0, (cudaStream_t)glb_cmd_queue >> > ((real*)x, (real*)xold1, (real*)xold2, (real*)df0dx, (real*)dfdx,
+        (real*)xlow, (real*)xupp, (real*)xmin, (real*)xmax,
+        (real*)alpha, (real*)beta, (real*)p0j, (real*)q0j, (real*)pij, (real*)qij, temp,
+        *asyinit, *asydecr, *asyincr,
+        *n, *m, *iter);
+
+
+
+
+    for (int i = 0; i < num2; i++) {
+        int nb = (num1 + 2048 - 1) / 2048;
+        mmasum_kernel <real> << <nb, 1024 >> > (temp, temp_sum, num1, i);
+
+        mmareduce_kernel<real> << <1, 1024, 0 >> > (temp_sum, nb);
+
+        cudaMemcpy((real*)bi + num2, temp_sum, sizeof(real), cudaMemcpyDeviceToDevice);
+    }
+    cudaFree(temp);
+    cudaFree(temp_sum);
+    ////global communication ?
+    /*
+    globaltmp_m = 0.0_rp
+    call MPI_Allreduce(this%bi%x, globaltmp_m, this%m, &
+         mpi_real_precision, mpi_sum, neko_comm, ierr)
+    this%bi%x = globaltmp_m - fval
+    */
 
 }
 
