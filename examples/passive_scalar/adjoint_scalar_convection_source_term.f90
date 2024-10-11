970a6e55d19e15272c5651f24a56d59e384d2945
gloopydoop <nobis@mech.kth.se>
2024-10-11 11:47:46 +0200

    merging in stuff from PDE filters

---
+++ examples/passive_scalar/adjoint_scalar_convection_source_term.f90
@@ -0,0 +1,157 @@
+! Copyright (c) 2023, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
+!> Implements the `adjoint_scalar_convection_source_term` type.
+! this is a such a dumb name
+module adjoint_scalar_convection_source_term
+  use num_types, only : rp
+  use field_list, only : field_list_t
+  use field, only: field_t
+  use field_registry, only: neko_field_registry
+  use scratch_registry, only: neko_scratch_registry
+  use json_module, only : json_file
+  use json_utils, only: json_get, json_get_or_default
+  use source_term, only : source_term_t
+  use coefs, only : coef_t
+  use neko_config, only : NEKO_BCKND_DEVICE
+  use utils, only : neko_error
+  implicit none
+  private
+
+  ! I don't know how to name this term, but when you have a passive
+  ! scalar you get an extra term in the adjoint velocity equation, which comes
+  ! from the convective term in the passive scalar equation.
+  ! Maybe this should be called just `adjoint_scalar_convection`? 
+  ! but it does come in a source term...
+
+  ! In any case,
+  ! it's a source term acting on the adjoint velocity equations, of the form:
+  ! $\nabla s s_adj$
+  type, public, extends(source_term_t) :: &
+  adjoint_scalar_convection_source_term_t
+   contains
+     !> The common constructor using a JSON object.
+     procedure, pass(this) :: init => &
+     adjoint_scalar_convection_source_term_init_from_json
+     !> The constructor from type components.
+     procedure, pass(this) :: init_from_compenents => &
+       adjoint_scalar_convection_source_term_init_from_components
+     !> Destructor.
+     procedure, pass(this) :: free => adjoint_scalar_convection_source_term_free
+     !> Computes the source term and adds the result to `fields`.
+     procedure, pass(this) :: compute_ => &
+     adjoint_scalar_convection_source_term_compute
+  end type adjoint_scalar_convection_source_term_t
+
+contains
+  !> The common constructor using a JSON object.
+  !! @param json The JSON object for the source.
+  !! @param fields A list of fields for adding the source values.
+  !! @param coef The SEM coeffs.
+  subroutine adjoint_scalar_convection_source_term_init_from_json(this, &
+  json, fields, coef)
+    class(adjoint_scalar_convection_source_term_t), intent(inout) :: this
+    type(json_file), intent(inout) :: json
+    type(field_list_t), intent(inout), target :: fields
+    type(coef_t), intent(inout), target :: coef
+    real(kind=rp), allocatable :: values(:)
+    real(kind=rp) :: start_time, end_time
+
+    ! we really don't have to initialize anything here...
+    ! maybe we should check that we have a passive scalar?
+
+  end subroutine adjoint_scalar_convection_source_term_init_from_json
+
+  subroutine adjoint_scalar_convection_source_term_init_from_components(this, &
+  fields, values, coef, start_time, end_time)
+    class(adjoint_scalar_convection_source_term_t), intent(inout) :: this
+    class(field_list_t), intent(inout), target :: fields
+    real(kind=rp), intent(in) :: values(:)
+    type(coef_t) :: coef
+    real(kind=rp), intent(in) :: start_time
+    real(kind=rp), intent(in) :: end_time
+
+    call this%free()
+    call this%init_base(fields, coef, start_time, end_time)
+  end subroutine adjoint_scalar_convection_source_term_init_from_components
+
+
+  !> Destructor.
+  subroutine adjoint_scalar_convection_source_term_free(this)
+    class(adjoint_scalar_convection_source_term_t), intent(inout) :: this
+
+    call this%free_base()
+  end subroutine adjoint_scalar_convection_source_term_free
+
+  !> Computes the source term and adds the result to `fields`.
+  !! @param t The time value.
+  !! @param tstep The current time-step.
+  subroutine adjoint_scalar_convection_source_term_compute(this, t, tstep)
+    class(adjoint_scalar_convection_source_term_t), intent(inout) :: this
+    real(kind=rp), intent(in) :: t
+    integer, intent(in) :: tstep
+    integer :: n_fields, i, n
+    type(field_t), pointer :: s, s_adj, fu, fv, fw
+    integer :: temp_indices(3)
+    type(field_t), pointer :: dsdx, dsdy, dsdz
+
+    
+    call neko_scratch_registry%request_field(dsdx, temp_indices(1))
+    call neko_scratch_registry%request_field(dsdy, temp_indices(2))
+    call neko_scratch_registry%request_field(dsdz, temp_indices(3))
+    
+    fu => this%fields%get(1)
+    fv => this%fields%get(2)
+    fw => this%fields%get(3)
+    
+    
+    s => neko_field_registry%get_field('s')
+    s_adj => neko_field_registry%get_field('s_adj')
+    
+    ! we basically just need the term 
+    ! $\nabla s s_adj$
+    ! TODO
+    ! be super careful with opgrad vs grad.
+    ! I'm not sure which is correct here
+    ! From memory they only differ by a jac_inv or a B or something like that
+    call opgrad(dsdx%x,dsdy%x,dsdz%x,s%x, this%coef)
+    ! TODO
+    ! double check if add or subtract
+    call field_addcol3(fu,s,dsdx)
+    call field_addcol3(fv,s,dsdy)
+    call field_addcol3(fw,s,dsdz)
+    
+    ! free the scratch
+    call neko_scratch_registry%relinquish_field(temp_indices)
+  end subroutine adjoint_scalar_convection_source_term_compute
+
+end module adjoint_scalar_convection_source_term
---
+++ examples/passive_scalar/driver.f90
@@ -0,0 +1,231 @@
+program usrneko
+  use neko, only: neko_init, neko_solve, neko_finalize
+  use case, only: case_t
+  use user, only: user_setup
+  use adjoint_case, only: adjoint_case_t, adjoint_init, adjoint_free
+  use simulation_adjoint, only: solve_adjoint
+
+
+  ! here are all the extra things i'll add to "problem"
+  use source_term, only: source_term_t
+  use source_term_handler, only: source_term_handler_t
+  use topopt_design, only: topopt_design_t
+  use simple_brinkman_source_term, only: simple_brinkman_source_term_t
+  use adjoint_minimum_dissipation_source_term, &
+       only: adjoint_minimum_dissipation_source_term_t
+  use objective_function, only: objective_function_t
+  use minimum_dissipation_objective_function, &
+       only: minimum_dissipation_objective_function_t
+  use field, only:field_t
+  use scratch_registry, only : neko_scratch_registry
+  use num_types, only : rp, sp, dp, qp
+  use field_math, only: field_rzero, field_rone, field_cmult
+  use volume_constraint, only: volume_constraint_t
+  use fld_file_output, only : fld_file_output_t
+  use steady_state_problem, only : steady_state_problem_t
+  use mma, only: mma_t
+  use neko_ext, only: reset
+  use math, only: copy, cmult
+
+
+  !> a problem type
+  type(steady_state_problem_t) :: problem
+  !> a design type
+  type(topopt_design_t) :: design
+  !> an optimizer (in this case mma)
+  type(mma_t) :: optimizer
+
+  ! these are some things needed for MMA/ work arrays (all these will become
+  ! redundant when we do this properly)
+  type(field_t), pointer :: wo1, wo2, wo3
+  integer :: temp_indices(3)
+  integer :: n, optimization_iteration
+  real(kind=rp), dimension(1) :: fval
+  real(kind=rp), allocatable :: x_switch(:)
+
+  ! these would live in "problem"
+  !> and primal case                                                         
+  type(case_t), public :: C                                                  
+  !> and adjoint case                                                        
+  type(adjoint_case_t), public :: adj
+  ! the brinkman source terms
+  type(simple_brinkman_source_term_t) :: forward_brinkman, adjoint_brinkman
+
+
+  ! init the problem (base)
+ !call problem%init_base()
+!!------------------------------------------------------------------------------
+    call user_setup(C%usr)
+    ! initialize the primal
+    call neko_init(C)
+    ! initialize the adjoint
+    call adjoint_init(adj, C)
+
+
+ ! init the design
+ ! call design%init(problem%C%params, problem%C%fluid%c_Xh)
+!!------------------------------------------------------------------------------
+  call design%init(C%params, C%fluid%c_Xh)
+
+
+  ! init the problem, with the design
+!  call problem%init_design(design)
+!!------------------------------------------------------------------------------
+   ! init the simple brinkman term for the forward problem
+    call forward_brinkman%init_from_components( &
+         C%fluid%f_x, C%fluid%f_y, C%fluid%f_z, &
+         design, &
+         C%fluid%u, C%fluid%v, C%fluid%w, &
+         C%fluid%c_Xh)
+    ! append brinkman source term to the forward problem
+    call C%fluid%source_term%add(forward_brinkman)
+
+    ! init the simple brinkman term for the adjoint
+    call adjoint_brinkman%init_from_components( &
+         adj%scheme%f_adj_x, adj%scheme%f_adj_y, &
+         adj%scheme%f_adj_z, &
+         design, &
+         adj%scheme%u_adj, adj%scheme%v_adj, adj%scheme%w_adj, &
+         adj%scheme%c_Xh)
+    ! append brinkman source term based on design
+    call adj%scheme%source_term%add(adjoint_brinkman)
+
+    ! NOW here is where we would initialize our new objective functions!
+
+
+
+!  ! TODO
+!  ! call `optimizer%init(problem, design)`
+!!------------------------------------------------------------------------------
+!  ! this all has to be wrapped up again!
+!  ! so that we have an `optimizer_t` class, where it would pick out MMA.
+!  ! so this would be:
+!
+!
+!  ! - `problem` knows how many constraints etc there are
+!  ! - `design` knows that we're doing topology optimization
+!  ! here we've already read through the JSON to find the number of constraints
+!
+!  ! here we hard code MMA
+!  ! Then we need to tell MMA how many constraints we need etc
+!  ! work arrays for xmin and xmax
+!  call neko_scratch_registry%request_field(wo1, temp_indices(1))
+!  call neko_scratch_registry%request_field(wo2, temp_indices(2))
+!  call field_rzero(wo1)
+!  call field_rone (wo2)
+!  ! obviously do this properly in the future...
+!  n = design%design_indicator%size()
+!  call optimizer%init_json(design%design_indicator%x, n, &
+!       1, 0.0_rp, (/0.0_rp/), (/100.0_rp/), (/0.0_rp/), wo1%x, wo2%x, problem%C%params)
+!  !m, a0         a_i          c_i           d_i
+!  ! -------------------------------------------------------------------!
+!  !      Internal parameters for MMA                                   !
+!  !      Minimize  f_0(x) + a_0*z + sum( c_i*y_i + 0.5*d_i*(y_i)^2 )   !
+!  !    subject to  f_i(x) - a_i*z - y_i <= 0,  i = 1,...,m             !
+!  !                xmin_j <= x_j <= xmax_j,    j = 1,...,n             !
+!  !                z >= 0,   y_i >= 0,         i = 1,...,m             !
+!  ! -------------------------------------------------------------------!
+!
+!  call neko_scratch_registry%relinquish_field(temp_indices)
+!!------------------------------------------------------------------------------
+!
+!
+!
+!  ! TODO
+!  ! call `optimizer%compute(problem, design)`
+!!------------------------------------------------------------------------------
+!  optimization_iteration = 1
+!  do while (optimization_iteration .lt. 100)
+!
+!     ! compute objective function
+!     call problem%compute()
+!
+!     ! in this case it's MMA so we need gradient information
+!     call problem%compute_sensitivity()
+!
+!     ! now we have the optimizer act on the design field.
+!
+!     fval(1) = problem%volume_constraint%objective_function_value
+!     associate(x => design%design_indicator%x, &
+!          df0dx=>design%sensitivity%x, &
+!          dfdx => problem%volume_constraint%sensitivity_to_coefficient%x)
+!       ! TODO
+!       ! reshape everything and use the propper "update"
+!
+!       ! TODO
+!       ! I'm CERTAIN the mass matrix comes in here, but I need to sit down
+!       ! with a pen and paper
+!
+!       ! TODO
+!       ! for heuristic reasons, it's important to rescale the dfdx etc so
+!       ! they're within a reasonable range unfortunately this takes a bit of
+!       ! trial and error, but we should include a subroutine that
+!       ! allows us to rescale
+!       ! call cmult(df0dx,0.01_rp,n)
+!       call cmult(dfdx,100.0_rp,n)
+!       fval(1) = fval(1)*100.0_rp
+!       ! (and also prints out some norms to make the trial and error and
+!       ! bit easier)
+!
+!       call problem%sample(real(optimization_iteration,rp))
+!
+!       !call optimizer%mma_update_cpu( &
+!       !     optimization_iteration, x, df0dx, fval, dfdx)
+!
+!       ! TODO
+!       ! this is a really dumb way of handling the reshaping..
+!       if( .not. allocated(x_switch)) then
+!       allocate(x_switch(optimizer%get_n()))
+!       end if
+!
+!       x_switch = reshape(x,[optimizer%get_n()])
+!
+!       call optimizer%mma_update_cpu( &
+!            optimization_iteration, &
+!            x_switch, &
+!            reshape(df0dx,[optimizer%get_n()]), &
+!            fval, &
+!            reshape(dfdx,[optimizer%get_m(), optimizer%get_n()]))
+!
+!		 call copy(x,x_switch,optimizer%get_n())
+!
+!       ! TODO
+!       ! do a KKT check and do a propper convergence check..
+!       call optimizer%kkt(x, df0dx, fval, dfdx)
+!
+!       ! TODO
+!       ! on the MMA side, we need residunorm public
+!       print *, 'KKT', optimizer%get_residunorm()
+!
+!     end associate
+!
+!     ! TODO
+!     ! instead of writing the objective function etc to the log file,
+!     ! we should come up with our own "optimization log" file, that writes out
+!     ! iterations, KKT, objective function values etc
+!
+!     optimization_iteration = optimization_iteration + 1
+!     call design%map_forward()
+!
+!     call reset(problem%C)
+!     ! TODO
+!     ! reset for the adjoint
+!     call field_rzero(problem%adj%scheme%u_adj)
+!     call field_rzero(problem%adj%scheme%v_adj)
+!     call field_rzero(problem%adj%scheme%w_adj)
+!
+!     ! don't forget to unfreeze the fluid!
+!     problem%C%fluid%freeze = .false.
+!
+!
+!
+!  end do
+!!------------------------------------------------------------------------------
+!
+!  deallocate(x_switch)
+!  call problem%free()
+!  call design%free()
+!  ! TODO
+!  call optimizer%free()
+
+end program usrneko
