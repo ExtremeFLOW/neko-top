3e3edabaccac6d95e682edf6e9ac8bc3f4b978f5
Tim Felle Olsen <tife@dtu.dk>
2024-10-21 13:42:28 +0200

    Remove the need for internal pointer to design

--- sources/problems/steady_state_problem.f90
+++ sources/problems/steady_state_problem.f90
@@ -64,8 +64,6 @@ module steady_state_problem
      !> and adjoint case
      type(adjoint_case_t), public :: adj
 
-     type(topopt_design_t), pointer :: design
-
      !> TODO
      ! we need a `objective_list` which is allocatable and contains a factory
      ! to fill itself up with from the JSON
@@ -155,11 +153,8 @@ contains
 
     type(simple_brinkman_source_term_t) :: forward_brinkman, adjoint_brinkman
 
-    ! Point the design to your own design
-    this%design => design
-
     ! init the design
-    call this%design%init(this%C%params, this%C%fluid%c_Xh)
+    call design%init(this%C%params, this%C%fluid%c_Xh)
 
     ! init the simple brinkman term for the forward problem
     call forward_brinkman%init_from_components( &
@@ -317,8 +312,8 @@ contains
     ! We would presumable have a list that holds all of objective functions
     ! and constraints, such that this would be a
     ! objectives%compute()
-    call this%objective_function%compute(this%design, this%C%fluid)
-    call this%volume_constraint%compute(this%design, this%C%fluid)
+    call this%objective_function%compute(design, this%C%fluid)
+    call this%volume_constraint%compute(design, this%C%fluid)
     print *, 'OBJECTIVE FUNCTION', &
          this%objective_function%objective_function_value
     print *, 'VOLUME CONSTRAINT', &
@@ -362,13 +357,13 @@ contains
     ! objectives%compute_sensitivity()
     ! and it would cycled through the list.
     call this%objective_function%compute_sensitivity(&
-         this%design, this%C%fluid, this%adj%scheme)
+         design, this%C%fluid, this%adj%scheme)
     call this%volume_constraint%compute_sensitivity(&
-         this%design, this%C%fluid, this%adj%scheme)
+         design, this%C%fluid, this%adj%scheme)
     ! it would be nice to visualize this
 
     ! do the adjoint mapping
-    call this%design%map_backward(&
+    call design%map_backward(&
          this%objective_function%sensitivity_to_coefficient)
     ! ok now you've fucked up the whole "list of sensitivity fields" aspect...
     ! we somehow need to populate the list
