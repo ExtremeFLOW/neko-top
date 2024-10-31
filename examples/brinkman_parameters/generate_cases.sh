ee01603313fa57594ce503f6db57e9f954707d02
Tim Felle Olsen <tife@dtu.dk>
2024-10-31 12:03:58 +0100

    merge the scripts

--- examples/brinkman_parameters/generate_cases.sh
+++ examples/brinkman_parameters/generate_cases.sh
@@ -104,3 +104,79 @@ case_name="Filter_radius"
 make_a_case mesh_list Re_list chi_list implicit_list radius_list $case_name
 
 # I still don't know exactly what the remaining cases should be..
+
+# omg Tim you're going to read this eventually and this it's such a stupid way of doing this hahahaha
+
+# Mesh		4
+# dt			10
+# Re			25
+# Chi			69
+# implcit	72
+# radius		76
+
+# mesh study
+declare -a mesh_list=("2" "3" "4")
+declare -a Re_list=("200")
+
+big_name="Mesh_study"
+rm -r $big_name 2>/dev/null
+mkdir $big_name
+
+for mesh in "${mesh_list[@]}"; do
+    for Re in "${Re_list[@]}"; do
+        name=$big_name/mesh${mesh}_Re${Re}
+        cp -r default_case_meshed $name
+        mv $name/case.template $name/cylinder.case
+        # now all the replacements
+
+        new_line='4s#.*#"mesh_file":"data_local/brinkman_parameters/meshed_M'$mesh'.nmsh",#'
+        sed -i $new_line $name/cylinder.case
+        new_line='25s#.*#"Re":'$Re',#'
+        sed -i $new_line $name/cylinder.case
+        # time step is a bit strange
+        case $mesh in
+        "2")
+            dt="2.5e-3"
+            ;;
+        "3")
+            dt="1.6e-3"
+            ;;
+        "4")
+            dt="1.25e-3"
+            ;;
+        esac
+        new_line='10s#.*#"timestep":'$dt',#'
+        sed -i $new_line $name/cylinder.case
+    done
+done
+
+# Reynolds study
+declare -a mesh_list=("3")
+declare -a Re_list=("200" "1000" "2000" "3900")
+
+big_name="Re_study"
+rm -r $big_name 2>/dev/null
+mkdir $big_name
+
+for mesh in "${mesh_list[@]}"; do
+    for Re in "${Re_list[@]}"; do
+        name=$big_name/mesh${mesh}_Re${Re}
+        cp -r default_case_meshed $name
+        mv $name/case.template $name/cylinder.case
+        # now all the replacements
+
+        new_line='4s#.*#"mesh_file":"data_local/brinkman_parameters/meshed_M'$mesh'.nmsh",#'
+        sed -i $new_line $name/cylinder.case
+        new_line='25s#.*#"Re":'$Re',#'
+        sed -i $new_line $name/cylinder.case
+        # time step is a bit strange
+        case $Re in
+        "200") dt="2.50e-3" ;;
+        "1000") dt="0.50e-3" ;;
+        "2000") dt="0.25e-3" ;;
+        "3900") dt="0.01e-3" ;;
+        esac
+        new_line='10s#.*#"timestep":'$dt',#'
+        sed -i $new_line $name/cylinder.case
+    done
+done
