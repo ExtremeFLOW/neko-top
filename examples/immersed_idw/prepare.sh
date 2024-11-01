0d0d662338b76cacd3c7538d1768cfe131766611
Tim Felle Olsen <tife@dtu.dk>
2024-11-01 13:59:36 +0100

    Now matches the scale of the other ibm examples

--- examples/immersed_idw/prepare.sh
+++ examples/immersed_idw/prepare.sh
@@ -1,3 +1,3 @@
-Nx=20 && Ny=20 && Nz=1
+Nx=80 && Ny=40 && Nz=1
 echo "Generating mesh with dimensions: $Nx $Ny $Nz"
-genmeshbox 0 1 0 1 0 1 $Nx $Ny $Nz .false. .true. .true.
+genmeshbox -15 35 -15 15 0 1 $Nx $Ny $Nz .false. .false. .true.
