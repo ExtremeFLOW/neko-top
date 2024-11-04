f2aaf6d6695eb9134c598c500bbd3af2566acb6b
Tim Felle Olsen <tife@dtu.dk>
2024-11-04 11:10:23 +0100

    cleanup user file

--- examples/brinkman_parameters/default_case/cylinder.f90
+++ examples/brinkman_parameters/default_case/cylinder.f90
@@ -13,28 +13,7 @@ contains
     type(user_t), intent(inout) :: user
     user%user_check => user_calc_quantities
     user%fluid_user_ic => user_ic
-    user%user_init_modules => initialize
   end subroutine user_setup
-
-  ! Initialize user variables or external objects
-  subroutine initialize(t, u, v, w, p, coef, params)
-    real(kind=rp) :: t
-    type(field_t), intent(inout) :: u
-    type(field_t), intent(inout) :: v
-    type(field_t), intent(inout) :: w
-    type(field_t), intent(inout) :: p
-    type(coef_t), intent(inout) :: coef
-    type(json_file), intent(inout) :: params
-  
-    call neko_field_registry%add_field(u%dof, "dudx", .true.)
-    call neko_field_registry%add_field(u%dof, "dudy", .true.)
-    call neko_field_registry%add_field(u%dof, "dvdx", .true.)
-    call neko_field_registry%add_field(u%dof, "dvdy", .true.)
-  
-  end subroutine initialize
-  
-
-
 
   ! User-defined routine called at the end of every time step
   subroutine user_calc_quantities(t, tstep, u, v, w, p, coef, params)
@@ -42,41 +21,23 @@ contains
     integer, intent(in) :: tstep
     type(coef_t), intent(inout) :: coef
     type(json_file), intent(inout) :: params
-    type(field_t), intent(inout) :: u
-    type(field_t), intent(inout) :: v
-    type(field_t), intent(inout) :: w
-    type(field_t), intent(inout) :: p
+    type(field_t), intent(inout) :: u, v, w, p
     type(field_t), pointer :: brinkman
-    type(field_t), pointer :: dudx, dudy, dudz
-    type(field_t), pointer :: dvdx, dvdy, dvdz
     type(field_t), pointer :: work
     integer :: temp_indices(1)
 
-    integer ntot
+    integer :: ntot
     real(kind=rp) :: leakage, lift, drag
     real(kind=rp) :: div_tot
 
-    ! This may be really inefficent... but we're going to need to probe the 
-    ! gradient if we want the viscous contribution to the drag/lift.
-    dudx => neko_field_registry%get_field("dudx")
-    dudy => neko_field_registry%get_field("dvdx")
-    dvdx => neko_field_registry%get_field("dudy")
-    dvdy => neko_field_registry%get_field("dvdy")
-
-    call dudxyz(dudx%x, u%x, coef%drdx, coef%dsdx, coef%dtdx, coef)
-    call dudxyz(dudy%x, u%x, coef%drdy, coef%dsdy, coef%dtdy, coef)
-    call dudxyz(dvdx%x, v%x, coef%drdx, coef%dsdx, coef%dtdx, coef)
-    call dudxyz(dvdy%x, v%x, coef%drdy, coef%dsdy, coef%dtdy, coef)
-
-    ! We need the Brinkman term in the registry 
-    call neko_field_registry%add_field(u%dof, "brinkman_indicator", .true.)
+    ! We need the Brinkman term in the registry
     brinkman => neko_field_registry%get_field("brinkman_indicator")
 
     ntot = u%dof%size()
 
     ! Another good metric inspired by
     ! A. Ghasemi & A. Elham (2019)
-    ! "FLOW TOPOLOGY OPTIMIZATION IN PERIODIC DOMAINS WITH APPLICATION TO 
+    ! "FLOW TOPOLOGY OPTIMIZATION IN PERIODIC DOMAINS WITH APPLICATION TO
     ! MICRO HEAT EXCHANGER OPTIMIZATION"
 
     ! I think they are aluding to that: we only have one source term (brinkman)
@@ -90,25 +51,15 @@ contains
     drag = glsc2(work%x, coef%B, ntot)
     call field_col3(work, brinkman, v)
     lift = glsc2(work%x, coef%B, ntot)
-
-
-
-
+    call neko_scratch_registry%relinquish_field(temp_indices)
 
     ! calculate the leakage
-    leakage = leak(brinkman%x,u%x,v%x,w%x,coef%b,ntot)
+    leakage = leak(brinkman%x, u%x, v%x, w%x, coef%b, ntot)
     if (pe_rank .eq. 0) then
        print *, 'Leakage = ', leakage, ',  ', t
        print *, 'Lift = ', lift, ',  ', t
        print *, 'Drag = ', drag, ',  ', t
-    endif
-
-
-
-
-
-
-
+    end if
 
   end subroutine user_calc_quantities
 
@@ -132,7 +83,6 @@ contains
 
   end function leak
 
-
   ! User-defined initial condition
   subroutine user_ic(u, v, w, p, params)
     type(field_t), intent(inout) :: u
@@ -148,11 +98,11 @@ contains
        w%x(i,1,1,1) = 1.0_rp
 
        ! just to break the symmetry and induce shedding quicker
-       if(abs(u%dof%y(i,1,1,1)).lt.4.0_rp) then
+       if (abs(u%dof%y(i,1,1,1)) .lt. 4.0_rp) then
           v%x(i,1,1,1) = 0.1_rp
        else
           v%x(i,1,1,1) = 0.0_rp
-       endif
+       end if
 
     end do
     p = 0._rp
