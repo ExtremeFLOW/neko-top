f1591c2bbb916acf2c2b00f64fc48d9d052701f3
Tim Felle Olsen <tife@dtu.dk>
2024-12-19 10:02:56 +0100

    Update to new bc list interface

--- sources/adjoint/adjoint_scheme.f90
+++ sources/adjoint/adjoint_scheme.f90
@@ -61,8 +61,8 @@ module adjoint_scheme
   use hsmg, only: hsmg_t
   use precon, only: pc_t, precon_factory, precon_destroy
   use fluid_stats, only: fluid_stats_t
-  use bc, only: bc_t, bc_list_t, bc_list_init, bc_list_add, bc_list_free, &
-       bc_list_apply_scalar, bc_list_apply_vector
+  use bc, only: bc_t
+  use bc_list, only: bc_list_t
   use mesh, only: mesh_t, NEKO_MSH_MAX_ZLBL_LEN, NEKO_MSH_MAX_ZLBLS
   use math, only: cfill, add2s2
   use device_math, only: device_cfill, device_add2s2
@@ -482,7 +482,7 @@ contains
             this%bc_labels)
     end if
 
-    call bc_list_init(this%bclst_vel)
+    call this%bclst_vel%init()
 
     call this%bc_sym%init_base(this%c_Xh)
     call this%bc_sym%mark_zone(msh%sympln)
@@ -490,7 +490,7 @@ contains
          'sym', this%bc_labels)
     call this%bc_sym%finalize()
     call this%bc_sym%init(this%c_Xh)
-    call bc_list_add(this%bclst_vel, this%bc_sym)
+    call this%bclst_vel%append(this%bc_sym)
 
     !
     ! Inflow
@@ -519,7 +519,7 @@ contains
        call this%bc_inflow%mark_zones_from_list(msh%labeled_zones,&
             'v', this%bc_labels)
        call this%bc_inflow%finalize()
-       call bc_list_add(this%bclst_vel, this%bc_inflow)
+       call this%bclst_vel%append(this%bc_inflow)
 
        !if (trim(string_val1) .eq. "uniform") then
        !   call json_get(params, 'case.fluid.inflow_condition.value', real_vec)
@@ -548,7 +548,7 @@ contains
     call this%bc_wall%mark_zones_from_list(msh%labeled_zones,&
          'w', this%bc_labels)
     call this%bc_wall%finalize()
-    call bc_list_add(this%bclst_vel, this%bc_wall)
+    call this%bclst_vel%append(this%bc_wall)
 
     ! TODO
     ! HARRY
@@ -601,7 +601,7 @@ contains
     call this%user_field_bc_vel%finalize()
 
     ! Add the field bc to velocity bcs
-    call bc_list_add(this%bclst_vel, this%user_field_bc_vel)
+    call this%bclst_vel%append(this%user_field_bc_vel)
 
     !
     ! Associate our field dirichlet update to the user one.
@@ -623,14 +623,11 @@ contains
     call this%user_field_bc_vel%field_list%assign_to_field(4, &
          this%user_field_bc_prs%field_bc)
 
-    call bc_list_init(this%user_field_bc_vel%bc_list, size = 4)
-    ! Note, bc_list_add only adds if the bc is not empty
-    call bc_list_add(this%user_field_bc_vel%bc_list, &
-         this%user_field_bc_vel%bc_u)
-    call bc_list_add(this%user_field_bc_vel%bc_list,&
-         this%user_field_bc_vel%bc_v)
-    call bc_list_add(this%user_field_bc_vel%bc_list,&
-         this%user_field_bc_vel%bc_w)
+    call this%user_field_bc_vel%bc_list%init(size = 4)
+    ! Note, only adds%append(if the bc is not empty
+    call this%user_field_bc_vel%bc_list%append(this%user_field_bc_vel%bc_u)
+    call this%user_field_bc_vel%bc_list%append(this%user_field_bc_vel%bc_v)
+    call this%user_field_bc_vel%bc_list%append(this%user_field_bc_vel%bc_w)
 
     !
     ! Check if we need to output boundary types to a separate field
@@ -730,7 +727,7 @@ contains
     ! Strict convergence for the velocity solver
     call json_get_or_default(params, 'case.fluid.strict_convergence', &
          this%strict_convergence, .false.)
-    
+
     ! Assign velocity fields
     call neko_field_registry%add_field(this%dm_Xh, 'u_adj')
     call neko_field_registry%add_field(this%dm_Xh, 'v_adj')
@@ -778,7 +775,7 @@ contains
     !
     ! Setup pressure boundary conditions
     !
-    call bc_list_init(this%bclst_prs)
+    call this%bclst_prs%init()
     call this%bc_prs%init_base(this%c_Xh)
     call this%bc_prs%mark_zones_from_list(msh%labeled_zones,&
          'o', this%bc_labels)
@@ -794,8 +791,8 @@ contains
          MPI_INTEGER, MPI_SUM, NEKO_COMM, ierr)
 
     if (integer_val .gt. 0) call this%user_field_bc_prs%init_field('d_pres')
-    call bc_list_add(this%bclst_prs, this%user_field_bc_prs)
-    call bc_list_add(this%user_field_bc_vel%bc_list, this%user_field_bc_prs)
+    call this%bclst_prs%append(this%user_field_bc_prs)
+    call this%user_field_bc_vel%bc_list%append(this%user_field_bc_prs)
 
     if (msh%outlet%size .gt. 0) then
        call this%bc_prs%mark_zone(msh%outlet)
@@ -806,7 +803,7 @@ contains
 
     call this%bc_prs%finalize()
     call this%bc_prs%set_g(0.0_rp)
-    call bc_list_add(this%bclst_prs, this%bc_prs)
+    call this%bclst_prs%append(this%bc_prs)
     call this%bc_dong%init_base(this%c_Xh)
     call this%bc_dong%mark_zones_from_list(msh%labeled_zones,&
          'o+dong', this%bc_labels)
@@ -817,7 +814,7 @@ contains
 
     call this%bc_dong%init(this%c_Xh, params)
 
-    call bc_list_add(this%bclst_prs, this%bc_dong)
+    call this%bclst_prs%append(this%bc_dong)
 
     ! Pressure solver
     ! hopefully we can do the same trick here as we will eventually do for the
@@ -919,7 +916,7 @@ contains
 
     call this%c_Xh%free()
 
-    call bc_list_free(this%bclst_vel)
+    call this%bclst_vel%free()
 
     call this%scratch%free()
 
@@ -962,14 +959,14 @@ contains
     class(adjoint_scheme_t), target, intent(inout) :: this
     ! Variables for retrieving json parameters
 
-    if ( (.not. associated(this%u_adj)) .or. &
+    if ((.not. associated(this%u_adj)) .or. &
          (.not. associated(this%v_adj)) .or. &
          (.not. associated(this%w_adj)) .or. &
          (.not. associated(this%p_adj))) then
        call neko_error('Fields are not registered')
     end if
 
-    if ( (.not. allocated(this%u_adj%x)) .or. &
+    if ((.not. allocated(this%u_adj%x)) .or. &
          (.not. allocated(this%v_adj%x)) .or. &
          (.not. allocated(this%w_adj%x)) .or. &
          (.not. allocated(this%p_adj%x))) then
@@ -1029,7 +1026,7 @@ contains
     real(kind=rp), intent(in) :: t
     integer, intent(in) :: tstep
 
-    call bc_list_apply_vector(this%bclst_vel,&
+    call this%bclst_vel%apply_vector(&
          this%u_adj%x, this%v_adj%x, this%w_adj%x, this%dm_Xh%size(), t, tstep)
 
   end subroutine adjoint_scheme_bc_apply_vel
@@ -1041,8 +1038,8 @@ contains
     real(kind=rp), intent(in) :: t
     integer, intent(in) :: tstep
 
-    call bc_list_apply_scalar(this%bclst_prs, this%p_adj%x, &
-         this%p_adj%dof%size(), t, tstep)
+    call this%bclst_prs%apply_scalar(this%p_adj%x, this%p_adj%dof%size(), &
+         t, tstep)
 
   end subroutine adjoint_scheme_bc_apply_prs
 
