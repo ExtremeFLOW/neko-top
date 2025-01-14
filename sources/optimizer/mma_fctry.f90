4fb0d8a725aa74541af1f13e966ffa28d9ab2570
Abbas Mousavi <smousavi@mech.kth.se>
2025-01-14 13:25:37 +0100

    Add mma_fctry submodule to initialize mma_t object based on the backend device.

---
+++ sources/optimizer/mma_fctry.f90
@@ -0,0 +1,36 @@
+!> Defines MMA factory for the mma_t
+submodule (mma) mma_fctry
+  use neko_config, only : NEKO_BCKND_DEVICE, NEKO_BCKND_SX
+  ! use pnpn_res_device, only : pnpn_prs_res_device_t, pnpn_vel_res_device_t
+  ! use pnpn_res_cpu, only : pnpn_prs_res_cpu_t, pnpn_vel_res_cpu_t
+  ! use pnpn_res_sx, only : pnpn_prs_res_sx_t, pnpn_vel_res_sx_t
+  implicit none
+
+contains
+
+  !> Factory for the mma_t
+  !! @details Only selects the compute backend.
+  !! @param object The object to be allocated by the factory.
+  module subroutine mma_factory(object)
+    class(mma_t), allocatable, intent(inout) :: object
+
+    if (allocated(object)) then
+       deallocate(object)
+    end if
+
+    if (NEKO_BCKND_SX .eq. 1) then
+    else if (NEKO_BCKND_DEVICE .eq. 1) then
+    else
+    end if
+
+    ! if (NEKO_BCKND_SX .eq. 1) then
+    !    allocate(pnpn_prs_res_sx_t::object)
+    ! else if (NEKO_BCKND_DEVICE .eq. 1) then
+    !    allocate(pnpn_prs_res_device_t::object)
+    ! else
+    !    allocate(pnpn_prs_res_cpu_t::object)
+    ! end if
+
+  end subroutine mma_factory
+
+end submodule mma_fctry
