bacc893527e11d94c0928a78ada77cbc8c190ef8
Tim Felle Olsen <timfelle@hotmail.com>
2025-01-21 16:09:01 +0100

    Enhance Brinkman flow examples and metrics computation (#64)
    This update adds the examples for Brinkman flow, adds new metrics
    computation features, and enhances the overall structure and robustness
    of the code. It includes updates to the generator script, improved error
    handling, and additional documentation for better usability.
    
    Depends on: 
    
    - [x] #67 
    
    Closes #60

---
+++ examples/brinkman_parameters/post_processing/metrics/compute_metric.py
@@ -0,0 +1,65 @@
+"""
+Compute a metric for a given case.
+
+This script is used to compute a metric for a given case. The metric is
+specified by the user and the case is provided as a dictionary. The script
+returns the computed metric.
+"""
+
+# Global imports
+import os
+
+# Local imports
+from .separation_angle import inflection_benchmark
+
+
+def compute_metric(case: dict, metric: str, **kwargs) -> float:
+    """
+    Compute a metric for a given case.
+
+    Parameters
+    ----------
+    case : dict
+        The case dictionary.
+    metric : str
+        The metric to compute.
+    kwargs : dict
+        The keyword arguments.
+
+    Returns
+    -------
+    float
+        The computed metric.
+    """
+
+    if metric == "Frequency of Inflection Points":
+
+        # Extract keyword arguments
+        cache_dir = kwargs.get("cache_dir", None)
+        cases_dir = kwargs.get("cases_dir", None)
+
+        case_dir = os.path.join(cases_dir, case["name"])
+
+        circ_radius = kwargs.get("circ_radius", None)
+        if circ_radius is None:
+            raise ValueError(f"circ_radius must be provided for {metric}")
+
+        file_name = os.path.join(case_dir, 'circ_' + circ_radius + '.csv')
+
+        benchmark_results = inflection_benchmark(file_name, cache_dir)
+
+        match kwargs.get("which", None):
+            case "0":
+                return benchmark_results["max_freq"][0]
+            case "1":
+                return benchmark_results["max_freq"][1]
+            case "2":
+                return benchmark_results["max_freq"][2]
+            case _:
+                raise ValueError(
+                    f"Please select 'which' inflection point to track: 0, 1, or 2."
+                )
+
+    else:
+        print(f"Metric {metric} not recognized.")
+        exit(1)
