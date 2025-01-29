9ecd9e0f14f5b8ad46fec9fb1851227784b11e88
Tim Felle Olsen <tife@dtu.dk>
2025-01-29 08:42:37 +0100

    very broken still

--- sources/adjoint/adjoint_pnpn_bc_fctry.f90
+++ sources/adjoint/adjoint_pnpn_bc_fctry.f90
@@ -82,26 +82,27 @@ contains
 
     call json_get(json, "type", type)
 
-    if ( (trim(type) .eq. "outflow") .or. &
-         (trim(type) .eq. "normal_outflow")) then
+    select case (trim(type))
+      case ("outflow", "normal_outflow")
        allocate(zero_dirichlet_t::object)
-    else if ((trim(type) .eq. "outflow+dong") .or. &
-         (trim(type) .eq. "normal_outflow+dong")) then
+      case ("outflow+dong", "normal_outflow+dong")
        allocate(dong_outflow_t::object)
-    else if (trim(type) .eq. "user_pressure") then
+      case ("user_pressure")
        allocate(field_dirichlet_t::object)
        select type(obj => object)
          type is(field_dirichlet_t)
           obj%update => user%user_dirichlet_update
           call json%add("field_name", scheme%p%name)
        end select
-    else
-       do i=1, size(ADJOINT_PNPN_KNOWN_BCS)
+      case default
+       ! Check if the type is known since not all bc types require modifications
+       ! to the pressure
+       do i = 1, size(ADJOINT_PNPN_KNOWN_BCS)
           if (trim(type) .eq. trim(ADJOINT_PNPN_KNOWN_BCS(i))) return
        end do
        call neko_type_error("adjoint_pnpn boundary conditions", type, &
             ADJOINT_PNPN_KNOWN_BCS)
-    end if
+    end select
 
     call json_get(json, "zone_index", zone_index)
     call object%init(coef, json)
