f4e562ef58732d9b72f2dc029bf6597d2eac5a04
gloopydoop <nobis@mech.kth.se>
2025-02-08 16:34:26 +0100

    linting?

--- sources/problems/problems/steady_state_problem.f90
+++ sources/problems/problems/steady_state_problem.f90
@@ -114,9 +114,9 @@ contains
     class(design_t), target, intent(inout) :: design
 
     select type(design)
-    type is(topopt_design_t)
+     type is(topopt_design_t)
        call this%init_design_topopt(design)
-    class default
+     class default
        call neko_error('Only topopt_design_t is supported for now')
     end select
 
