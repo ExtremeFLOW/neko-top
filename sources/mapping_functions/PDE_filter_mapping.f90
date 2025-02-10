5b7e7d40ea617565ce99d9eb36819afe65c900c2
Harrison Nobis <58177854+gloopydoop@users.noreply.github.com>
2025-02-10 15:36:54 +0100

    Update sources/mapping_functions/PDE_filter_mapping.f90
    Co-authored-by: Tim Felle Olsen <tife@dtu.dk>

--- sources/mapping_functions/PDE_filter_mapping.f90
+++ sources/mapping_functions/PDE_filter_mapping.f90
@@ -223,11 +223,10 @@ contains
        call device_cfill(this%coef%h1_d, this%r**2, n)
        call device_cfill(this%coef%h2_d, 1.0_rp, n)
     else
-       do i = 1, n
-          ! h1 is already negative in its definition
-          this%coef%h1(i,1,1,1) = this%r**2
-          ! ax_helm includes the mass matrix in h2
-          this%coef%h2(i,1,1,1) = 1.0_rp
+       ! h1 is already negative in its definition
+       this%coef%h1 = this%r**2
+       ! ax_helm includes the mass matrix in h2
+       this%coef%h2 = 1.0_rp
        end do
     end if
     this%coef%ifh2 = .true.
