96b90af801fb3eff842a876b5d18858566c8a5a5
Tim Felle Olsen <tife@dtu.dk>
2025-02-11 16:18:18 +0100

    Improve the driver structure.

--- examples/rugby_ball/driver.f90
+++ examples/rugby_ball/driver.f90
@@ -2,19 +2,24 @@ program usrneko
   use mma_optimizer, only : mma_optimizer_t
   use steady_state_problem, only: steady_state_problem_t
   use topopt_design, only: topopt_design_t
+
   use num_types, only : rp
-  use mask_ops, only: mask_exterior_const
   use json_module, only: json_file
-  use mpi_f08, only: MPI_COMM_WORLD, MPI_Init, MPI_Comm_rank
   use utils, only: neko_error
   use json_utils_ext, only: json_read_file
 
+  use mpi_f08, only: MPI_Init, MPI_Finalize, MPI_Comm_rank, MPI_COMM_WORLD
+
+  implicit none
+
+  ! JSON related arguments
   integer :: argc
   type(json_file) :: parameters
   character(len=256) :: parameter_file
   real(kind=rp) :: tolerance
 
-  integer :: mpi_rank, ierr
+  ! MPI parameters
+  integer :: mpi_rank, ierr, max_iter
 
   !> The problem type
   type(steady_state_problem_t) :: problem
@@ -23,16 +28,24 @@ program usrneko
   !> The optimizer (in this case mma)
   type(mma_optimizer_t) :: optimizer
 
+  ! -------------------------------------------------------------------------- !
+  ! Initialize the MPI environment
+
   call MPI_Init(ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank, ierr)
 
+  ! -------------------------------------------------------------------------- !
   ! Read the parameters file as the first terminal argument
+
   argc = command_argument_count()
   if (argc .lt. 1) call neko_error('Missing parameter file')
   call get_command_argument(1, parameter_file)
 
   ! Read the parameters file
   parameters = json_read_file(trim(parameter_file))
+
+  ! -------------------------------------------------------------------------- !
+  ! Initialization of the components
 
   ! init the problem (base)
   call problem%init()
@@ -43,9 +56,15 @@ program usrneko
   ! init the optimizer
   call optimizer%init(problem, design)
 
+  ! -------------------------------------------------------------------------- !
+  ! Execute the optimization
+
   tolerance = 1.0e-3_rp
   max_iter = 100
   call optimizer%run(problem, tolerance, max_iter)
+
+  ! -------------------------------------------------------------------------- !
+  ! Clean up the components
 
   call problem%free()
   call design%free()
--- examples/rugby_verification/driver.f90
+++ examples/rugby_verification/driver.f90
@@ -2,17 +2,50 @@ program usrneko
   use mma_optimizer, only : mma_optimizer_t
   use steady_state_problem, only: steady_state_problem_t
   use topopt_design, only: topopt_design_t
-  use num_types, only : rp
-  use mask_ops, only: mask_exterior_const
 
+  use num_types, only : rp
+  use json_module, only: json_file
+  use utils, only: neko_error
+  use json_utils_ext, only: json_read_file
+
+  use mpi_f08, only: MPI_Init, MPI_Finalize, MPI_Comm_rank, MPI_COMM_WORLD
+
+  implicit none
+
+  ! JSON related arguments
+  integer :: argc
+  type(json_file) :: parameters
+  character(len=256) :: parameter_file
   real(kind=rp) :: tolerance
 
-  !> a problem type
+  ! MPI parameters
+  integer :: mpi_rank, ierr, max_iter
+
+  !> The problem type
   type(steady_state_problem_t) :: problem
-  !> a design type
+  !> The design type
   type(topopt_design_t) :: design
-  !> an optimizer (in this case mma)
+  !> The optimizer (in this case mma)
   type(mma_optimizer_t) :: optimizer
+
+  ! -------------------------------------------------------------------------- !
+  ! Initialize the MPI environment
+
+  call MPI_Init(ierr)
+  call MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank, ierr)
+
+  ! -------------------------------------------------------------------------- !
+  ! Read the parameters file as the first terminal argument
+
+  argc = command_argument_count()
+  if (argc .lt. 1) call neko_error('Missing parameter file')
+  call get_command_argument(1, parameter_file)
+
+  ! Read the parameters file
+  parameters = json_read_file(trim(parameter_file))
+
+  ! -------------------------------------------------------------------------- !
+  ! Initialization of the components
 
   ! init the problem (base)
   call problem%init()
@@ -23,10 +56,15 @@ program usrneko
   ! init the optimizer
   call optimizer%init(problem, design)
 
+  ! -------------------------------------------------------------------------- !
+  ! Execute the optimization
 
   tolerance = 1.0e-3_rp
   max_iter = 5
   call optimizer%run(problem, tolerance, max_iter)
+
+  ! -------------------------------------------------------------------------- !
+  ! Clean up the components
 
   call problem%free()
   call design%free()
