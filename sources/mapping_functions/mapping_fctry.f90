c2c870f78a46ce15207a7d951070cff9d217b16b
gloopydoop <nobis@mech.kth.se>
2025-03-12 16:00:42 +0100

    forgotten files

---
+++ sources/mapping_functions/mapping_fctry.f90
@@ -0,0 +1,82 @@
+! Copyright (c) 2023-2025, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
+!
+!> Defines a factory subroutine for mapping functions.
+submodule (mapping) mapping_fctry
+  use linear_mapping, only : linear_mapping_t
+  use PDE_filter, only: PDE_filter_t
+  use RAMP_mapping, only: RAMP_mapping_t
+  use json_utils, only : json_get
+  use utils, only : concat_string_array, neko_error
+  implicit none
+
+  ! List of all possible types created by the factory routine
+  character(len=20) :: MAPPING_KNOWN_TYPES(3) = [character(len=20) :: &
+     "linear", &
+     "PDE_filter", &
+     "RAMP"]
+
+contains
+
+  !> Mapping factory. Both constructs and initializes the object.
+  !! @param json JSON object initializing the mapping.
+  !! @param coef The SEM coefficients.
+  module subroutine mapping_factory(object, json, coef)
+    class(mapping_t), allocatable, intent(inout) :: object
+    type(json_file), intent(inout) :: json
+    type(coef_t), intent(inout) :: coef
+    character(len=:), allocatable :: type_name
+    character(len=:), allocatable :: type_string
+
+    call json_get(json, "type", type_name)
+
+    if (trim(type_name) .eq. "linear") then
+       allocate(linear_mapping_t::object)
+    else if (trim(type_name) .eq. "PDE_filter") then
+       allocate(PDE_filter_t::object)
+    else if (trim(type_name) .eq. "RAMP") then
+       allocate(RAMP_mapping_t::object)
+    else
+       type_string =  concat_string_array(MAPPING_KNOWN_TYPES, &
+            NEW_LINE('A') // "-  ", .true.)
+       call neko_error("Unknown mapping type: " &
+                       // trim(type_name) // ".  Known types are: " &
+                       // type_string)
+    end if
+
+    ! Initialize
+    call object%init(json, coef)
+
+  end subroutine mapping_factory
+
+end submodule mapping_fctry
