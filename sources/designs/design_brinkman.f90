226f8c16b5d9207f41def3b757210087db2c1b9e
Harrison Nobis <58177854+gloopydoop@users.noreply.github.com>
2025-03-31 11:20:04 +0200

    Passive scalar support (#110)
    This PR contains:
    
    -  Adjoint passive scalar support
    - A scalar mixing objective function
    - An example of a mixer
    
    ---------
    
    Co-authored-by: Tim Felle Olsen <tife@dtu.dk>
    Co-authored-by: Abbas Mousavi <smousavi@kth.se>
    Co-authored-by: Tim Felle Olsen <timfelle@hotmail.com>

--- sources/designs/design_brinkman.f90
+++ sources/designs/design_brinkman.f90
@@ -320,12 +320,9 @@ contains
     this%design_indicator%x = 0.0_rp
 
     n = this%design_indicator%dof%size()
+    ! This is probably getting fixed in tim's PR anyway, otherwise I'll fix it.
     do i = 1, n
-       if (sqrt((this%design_indicator%dof%x(i,1,1,1) - 0.5_rp)**2 + &
-            (this%design_indicator%dof%y(i,1,1,1) &
-            - 0.5_rp)**2) .lt. 0.25_rp) then
-          this%design_indicator%x(i,1,1,1) = 1.0_rp
-       end if
+       this%design_indicator%x(i,1,1,1) = 0.0_rp
     end do
 
     ! again this will be handled better in the future...
@@ -386,29 +383,29 @@ contains
 
     ! init the simple brinkman term for the forward problem
     call forward_brinkman%init_from_components( &
-         simulation%fluid_scheme%f_x, &
-         simulation%fluid_scheme%f_y, &
-         simulation%fluid_scheme%f_z, &
+         simulation%fluid%f_x, &
+         simulation%fluid%f_y, &
+         simulation%fluid%f_z, &
          this%brinkman_amplitude, &
-         simulation%fluid_scheme%u, &
-         simulation%fluid_scheme%v, &
-         simulation%fluid_scheme%w, &
-         simulation%fluid_scheme%c_Xh)
+         simulation%fluid%u, &
+         simulation%fluid%v, &
+         simulation%fluid%w, &
+         simulation%fluid%c_Xh)
     ! append brinkman source term to the forward problem
-    call simulation%fluid_scheme%source_term%add(forward_brinkman)
+    call simulation%fluid%source_term%add(forward_brinkman)
 
     ! init the simple brinkman term for the adjoint
     call adjoint_brinkman%init_from_components( &
-         simulation%adjoint_case%scheme%f_adj_x, &
-         simulation%adjoint_case%scheme%f_adj_y, &
-         simulation%adjoint_case%scheme%f_adj_z, &
+         simulation%adjoint_fluid%f_adj_x, &
+         simulation%adjoint_fluid%f_adj_y, &
+         simulation%adjoint_fluid%f_adj_z, &
          this%brinkman_amplitude, &
-         simulation%adjoint_case%scheme%u_adj, &
-         simulation%adjoint_case%scheme%v_adj, &
-         simulation%adjoint_case%scheme%w_adj, &
-         simulation%adjoint_case%scheme%c_Xh)
+         simulation%adjoint_fluid%u_adj, &
+         simulation%adjoint_fluid%v_adj, &
+         simulation%adjoint_fluid%w_adj, &
+         simulation%adjoint_fluid%c_Xh)
     ! append brinkman source term based on design
-    call simulation%adjoint_case%scheme%source_term%add(adjoint_brinkman)
+    call simulation%adjoint_fluid%source_term%add(adjoint_brinkman)
 
   end subroutine brinkman_design_init_from_components
 
