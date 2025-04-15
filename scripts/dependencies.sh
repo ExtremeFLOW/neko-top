fb41694e4151530e971f9171836997c68c6c3582
Tim Felle Olsen <tife@dtu.dk>
2025-04-15 13:04:12 +0200

    Move to old assignment of prefix upon installation

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -47,13 +47,12 @@ function find_json_fortran() {
     # Ensure JSON-Fortran is installed, if not install it.
     if [[ -z "$(find $JSON_FORTRAN_DIR -name libjsonfortran.so)" ]]; then
         cmake -S $JSON_FORTRAN_DIR -B $JSON_FORTRAN_DIR/build \
-            --install-prefix $JSON_FORTRAN_DIR \
             -Wno-dev \
             -DUSE_GNU_INSTALL_CONVENTION=ON \
             -DSKIP_DOC_GEN=ON
 
         cmake --build $JSON_FORTRAN_DIR/build --parallel
-        cmake --install $JSON_FORTRAN_DIR/build
+        cmake --install $JSON_FORTRAN_DIR/build --prefix $JSON_FORTRAN_DIR
         rm -fr $JSON_FORTRAN_DIR/build
     fi
 
@@ -230,13 +229,13 @@ function find_hdf5() {
 
     # Ensure HDF5 is installed, if not install it.
     if [[ -z "$(find $HDF5_DIR -name libhdf5.so)" ]]; then
-        cmake -B $HDF5_DIR/build -S $HDF5_DIR --install-prefix $HDF5_DIR \
+        cmake -B $HDF5_DIR/build -S $HDF5_DIR \
             -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx \
             -DCMAKE_Fortran_COMPILER=mpifort -DHDF5_ENABLE_PARALLEL=ON \
             -DHDF5_BUILD_FORTRAN=ON -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF \
             -DCMAKE_BUILD_TYPE=Release
         cmake --build $HDF5_DIR/build/ --config Release --parallel
-        cmake --install $HDF5_DIR/build/
+        cmake --install $HDF5_DIR/build/ --prefix $HDF5_DIR
         rm -fr $HDF5_DIR/build
     fi
 
