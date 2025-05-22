6c5bc0185ddf26e0fbf66eefdbbe7213153036cd
Tim Felle Olsen <tife@dtu.dk>
2025-05-22 10:28:40 +0300

    Do not assume the user intended to change the backend

--- setup.sh
+++ setup.sh
@@ -81,8 +81,8 @@ done
 if [ -f "$MAIN_DIR/build/CMakeCache.txt" ]; then
     CURRENT_DEVICE_TYPE="$(grep -oP '(?<=DEVICE_TYPE:STRING=).*' $MAIN_DIR/build/CMakeCache.txt)"
     if [ "$DEVICE_TYPE" != "$CURRENT_DEVICE_TYPE" ]; then
-        echo "Device type has changed, cleaning the build directory"
-        CLEAN=true
+        echo >&2 "Device type has changed. Please do a clean setup."
+        exit 1
     fi
 fi
 
