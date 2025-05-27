dcc9dc8c6b8ea24f336142f4988bca08f3b057a6
Tim Felle Olsen <tife@dtu.dk>
2025-05-27 10:59:42 +0200

    Enhancement/type_injection (#206)
    Utilize the new type injection system in Neko.
    
    This solves the user_simcomp bug spotted by harry.

--- sources/drivers/neko-user.f90
+++ sources/drivers/neko-user.f90
@@ -2,9 +2,11 @@ program usrneko
   use neko, only: neko_init, neko_solve, neko_finalize
   use case, only: case_t
   use user, only: user_setup
+  use neko_top, only: neko_top_register_types
 
   type(case_t), target :: C
 
+  call neko_top_register_types()
   call user_setup(C%usr)
   call neko_init(C)
   call neko_solve(C)
