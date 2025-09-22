9400ec854581bdd3827802520f6783affaf48edf
gloopydoop <nobis@mech.kth.se>
2025-09-22 11:43:47 +0200

    extra fixes

--- sources/problems/objectives/minimum_dissipation_objective.f90
+++ sources/problems/objectives/minimum_dissipation_objective.f90
@@ -81,7 +81,7 @@ module minimum_dissipation_objective
   use design, only: design_t
   use brinkman_design, only: brinkman_design_t
   use point_zone, only: point_zone_t
-  use mask_ops, only: mask_exterior_const
+  use mask_ops, only: mask_exterior_const, compute_masked_volume
   use math_ext, only: glsc2_mask
   use utils, only: neko_error
   use json_module, only: json_file
@@ -102,13 +102,14 @@ module minimum_dissipation_objective
      type(field_t), pointer :: w => null()
      !> Pointer to the coefficient field.
      type(coef_t), pointer :: c_Xh => null()
-
      !> Pointer to adjoint u field.
      type(field_t), pointer :: adjoint_u => null()
      !> Pointer to adjoint v field.
      type(field_t), pointer :: adjoint_v => null()
      !> Pointer to adjoint w field.
      type(field_t), pointer :: adjoint_w => null()
+     !> Volume of the objective domain.
+     real(kind=rp) :: volume
 
    contains
      !> The common constructor using a JSON object.
@@ -167,7 +168,6 @@ contains
     real(kind=rp), intent(in) :: weight
     character(len=*), intent(in) :: name
     character(len=*), intent(in) :: mask_name
-
     type(adjoint_minimum_dissipation_source_term_t) :: adjoint_forcing
 
     call this%init_base(name, design%size(), weight, mask_name)
@@ -181,6 +181,13 @@ contains
     this%adjoint_v => neko_field_registry%get_field('v_adj')
     this%adjoint_w => neko_field_registry%get_field('w_adj')
 
+    ! compute the volume of the objective domain
+    if (this%has_mask) then
+       this%volume = compute_masked_volume(this%mask, this%c_Xh)
+    else
+       this%volume = this%c_Xh%volume
+    end if
+
     ! you will need to init this!
     ! append a source term based on the minimum dissipation
     ! init the adjoint forcing term for the adjoint
@@ -190,7 +197,7 @@ contains
          simulation%adjoint_fluid%f_adj_z, &
          this%u, this%v, this%w, this%weight, &
          this%mask, this%has_mask, &
-         this%c_Xh)
+         this%c_Xh, this%volume)
 
     ! append adjoint forcing term based on objective function
     select type (f => simulation%adjoint_fluid)
