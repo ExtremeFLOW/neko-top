b6eb4285443021994c159dcad98ac31df56e745b
gloopydoop <nobis@mech.kth.se>
2025-09-24 09:38:03 +0200

    found the bug

--- tests/unit/sensitivity/problem_tester.f90
+++ tests/unit/sensitivity/problem_tester.f90
@@ -41,7 +41,7 @@ program minimum_dissipation_sensitivity
   ! Test specific variables
   real(kind=rp) :: tolerance = 1e-5_rp
   real(kind=rp), parameter :: perturbations(8) = [ &
-       5e-1_rp, 1e-1_rp, 5e-2_rp, 1e-2_rp, 5e-3_rp, 1e-3_rp, 5e-4_rp, 1e-4_rp]
+       5e-3_rp, 1e-3_rp, 5e-4_rp, 1e-4_rp, 5e-5_rp, 1e-5_rp, 5e-6_rp, 1e-6_rp]
 
   type(vector_t) :: sensitivities
   type(matrix_t) :: constraint_sensitivity
