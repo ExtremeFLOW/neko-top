4a6769759b3ee47d6315e28cf96bebfa177e8148
Tim Felle Olsen <tife@dtu.dk>
2025-09-25 10:20:20 +0200

    Move to template

---
+++ examples/static_mixer/create_examples.sh
@@ -0,0 +1,27 @@
+mkdir -p data_local/static_mixer
+
+mesh_pattern=mixer
+data_path=data_local/static_mixer
+example_path=examples/static_mixer
+
+N=(8 16 32 64)
+
+for n in ${N[@]}; do
+    Nx=$n
+    Ny=$((n / 4))
+    Nz=$((n / 4))
+    output_file="${data_path}/${mesh_pattern}_${Nx}x${Ny}x${Nz}.nmsh"
+
+    if [ ! -f ${output_file} ]; then
+    ./mesh.sh -b 0 4 0 1 0 1 $Nx $Ny $Nz
+    mv data_local/box.nmsh ${output_file}
+    fi
+
+    # Create the cases from the templates
+    cp ${example_path}/petsc/case.template ${example_path}/petsc/${n}.case
+    sed -i "s|\"mesh_file\": .*|\"mesh_file\": \"${output_file}\",|g" ${example_path}/petsc/${n}.case
+    sed -i "s|\"cache_file\": .*|\"cache_file\": \"${data_path}/petsc/cache_${n}_\",|g" ${example_path}/petsc/${n}.case
+
+    cp ${example_path}/neko_top/case.template ${example_path}/neko_top/${n}.case
+    sed -i "s|\"mesh_file\": .*|\"mesh_file\": \"${output_file}\",|g" ${example_path}/neko_top/${n}.case
+done
--- examples/static_mixer/petsc/mixer.f90
+++ examples/static_mixer/petsc/mixer.f90
@@ -8,6 +8,7 @@ contains
   subroutine user_setup(user)
     type(user_t), intent(inout) :: user
     user%initial_conditions => initial_conditions
+    user%dirichlet_conditions => dirichlet_update
   end subroutine user_setup
 
   !> User initial condition
@@ -26,4 +27,32 @@ contains
     call scalar_z_split_ic(s, 0.5_rp)
   end subroutine initial_conditions
 
+  subroutine dirichlet_update(fields, bc, time)
+    type(field_list_t), intent(inout) :: fields
+    type(field_dirichlet_t), intent(in) :: bc
+    type(time_state_t), intent(in) :: time
+
+    type(field_t), pointer :: s
+    integer :: i
+    real(kind=rp) :: z
+
+    if (time%tstep .ne. 1) return
+
+    ! ------------------------------------------------------------------------ #
+    ! Assign the boundary condition for the scalar field here.
+
+    s => fields%get("temperature")
+
+    do i = 1, bc%msk(0)
+       z = bc%dof%z(bc%msk(i), 1, 1, 1)
+       if (z .gt. 0.5_rp) then
+          s%x(bc%msk(i), 1, 1, 1) = 1.0_rp
+       else
+          s%x(bc%msk(i), 1, 1, 1) = 0.0_rp
+       end if
+    end do
+
+    call s%copy_from(HOST_TO_DEVICE, .true.)
+
+  end subroutine dirichlet_update
 end module user
