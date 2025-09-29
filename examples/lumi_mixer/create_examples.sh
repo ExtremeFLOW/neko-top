43a17c1a5389cb5b6b87290f891603ae1205b722
Tim Felle Olsen <tife@dtu.dk>
2025-09-29 14:46:29 +0200

    cleanup

--- examples/lumi_mixer/create_examples.sh
+++ examples/lumi_mixer/create_examples.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Global parameters
-export MAIN_DIR=$(realpath $(dirname $0)/../..)
+export ROOT_DIR=$(realpath $(dirname $0))
+export MAIN_DIR=$(realpath $ROOT_DIR/../..)
+
 export mesh_pattern="mixer"
 export data_path=$(realpath "${MAIN_DIR}/data_local/static_mixer")
-export example_path=$(realpath "${MAIN_DIR}/examples/static_mixer/cases")
-export template_path=$(realpath "${MAIN_DIR}/examples/static_mixer/templates")
+export example_path=$(realpath "${MAIN_DIR}/examples/lumi_mixer/cases")
+export template_path=$(realpath "${MAIN_DIR}/examples/lumi_mixer/templates")
 
 [ ! -d ${data_path} ] && mkdir -p ${data_path}
 [ ! -d ${example_path} ] && mkdir -p ${example_path}
@@ -27,8 +29,7 @@ function create_case() {
 
     # Set file names
     local mesh_file="${data_path}/${mesh_pattern}_${Nx}x${Ny}x${Nz}.nmsh"
-    local case_path="${example_path}/${case_template}"
-    local case_file="${case_path}/${n}.case"
+    local case_file="${example_path}/${n}.case"
 
     # Create the mesh if it does not exist
     if [ ! -f ${mesh_file} ]; then
@@ -36,30 +37,13 @@ function create_case() {
             -o ${data_path} -f ${mesh_pattern}_${Nx}x${Ny}x${Nz}.nmsh
     fi
 
-    # Copy over the CMakeFile
-    [ ! -d ${case_path} ] && mkdir -p ${case_path}
-    if [ ! -f ${case_path}/CMakeLists.txt ]; then
-        cp ${template_path}/CMakeLists.${case_template} ${case_path}/CMakeLists.txt
-    fi
-
-    # Copy over the mixer.f90 file
-    if [ ! -f ${case_path}/mixer.f90 ]; then
-        cp ${template_path}/mixer.f90 ${case_path}/mixer.f90
-    fi
-
-
     # Create the cases from the templates
-    cp ${template_path}/case.${case_template} ${case_file}
+    cp ${template_path}/case.template ${case_file}
     sed -i "s|\"mesh_file\": .*|\"mesh_file\": \"${mesh_file}\",|g" ${case_file}
-
-    if [ ${case_template} == "petsc" ]; then
-        sed -i "s|\"cache_file\": .*|\"cache_file\": \"${data_path}/petsc/cache_${n}_\",|g" ${case_file}
-    fi
 }
 
-N=(4)
+N=(4 8 16)
 
 for n in ${N[@]}; do
-    create_case petsc ${n}
     create_case neko_top ${n}
 done
