7c93c0d0178f061e49208ad8c6c4a0253b86ae9c
Tim Felle Olsen <tife@dtu.dk>
2025-09-29 11:15:00 +0200

    Update example

--- examples/static_mixer/create_examples.sh
+++ examples/static_mixer/create_examples.sh
@@ -6,7 +6,7 @@ mesh_pattern=mixer
 data_path=data_local/static_mixer
 example_path=examples/static_mixer
 
-N=(8 16 32 64 128 256 512 1024 2048 4096)
+N=(8 16 32)
 
 for n in ${N[@]}; do
     Nx=$n
@@ -15,8 +15,7 @@ for n in ${N[@]}; do
     output_file="${data_path}/${mesh_pattern}_${Nx}x${Ny}x${Nz}.nmsh"
 
     if [ ! -f ${output_file} ]; then
-        ./mesh.sh -b 0 4 0 1 0 1 $Nx $Ny $Nz
-         \
+        ./mesh.sh -b 0 4 0 1 0 1 $Nx $Ny $Nz \
             -o ${data_path} -f ${mesh_pattern}_${Nx}x${Ny}x${Nz}.nmsh
     fi
 
