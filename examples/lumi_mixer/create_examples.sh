7ea3acadd5179c05963dd15aa8c982111ff4170d
Tim Felle Olsen <tife@dtu.dk>
2025-10-07 09:31:54 +0300

    Remove examples that were too large

--- examples/lumi_mixer/create_examples.sh
+++ examples/lumi_mixer/create_examples.sh
@@ -101,9 +101,6 @@ create_case $experiment  16   4   4 ${cluster} 1
 create_case $experiment  32   8   8 ${cluster} 1
 create_case $experiment  64  16  16 ${cluster} 1
 create_case $experiment 128  32  32 ${cluster} 1
-create_case $experiment 256  32  32 ${cluster} 1
-create_case $experiment 256  64  32 ${cluster} 1
-create_case $experiment 256  64  64 ${cluster} 1
 
 # Check that time is not affected by which dimension is scaled
 experiment="dimension_scaling"
