4b9c5f0b263efe6a0fcaa34f933bc5979b952fa1
Tim Felle Olsen <tife@dtu.dk>
2025-10-09 09:12:04 +0200

    Increase brinkman value

--- examples/static_mixer/create_examples.sh
+++ examples/static_mixer/create_examples.sh
@@ -34,6 +34,12 @@ for pe in ${Pe[@]}; do
             sed -i "s|\"mesh_file\": .*|\"mesh_file\": \"${output_file}\",|g" ${case_file}
             sed -i "s|\"cache_file\": .*|\"cache_file\": \"${data_path}/petsc/cache_${n}_\",|g" ${case_file}
 
+            if [ "$re" == 1 ]; then
+                sed -i "s|\"limits\": .*|\"limits\": [0.0, 5000.0],|g" ${case_file}
+            else
+                sed -i "s|\"limits\": .*|\"limits\": [0.0, 1000.0],|g" ${case_file}
+            fi
+
             if [[ "$1" == "mesh" && ! -f "${cache_file}" ]]; then
                 sed -i "s|\"end_time\": .*|\"end_time\": 1e-3,|g" ${case_file}
                 ./run.sh -c static_mixer/petsc/${n}_re_${re}_pe_${pe}.case
