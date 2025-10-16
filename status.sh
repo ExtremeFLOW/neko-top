615a846bb1520c8954b1d1e271358afdfcc8490f
Tim Felle Olsen <tife@dtu.dk>
2025-10-16 16:20:57 +0300

    status will also cleanup

--- status.sh
+++ status.sh
@@ -62,6 +62,7 @@ printf "\n\e[4mTest status.\e[m\n"
 for test in ${tests[@]}; do
     if [[ -d $RPATH/$test && ! -s $LPATH/$test/output.log && ! -s $LPATH/$test/error.log ]]; then
         printf '\t\e[1;32m%-12s\e[m %-s\n' "Complete:" "$test"
+        rm -rf $LPATH/$test
     fi
 done
 
