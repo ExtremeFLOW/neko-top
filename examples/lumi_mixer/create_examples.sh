bd693b2d519bb364b74f51949d4b4834ce5cf55e
Tim Felle Olsen <tife@dtu.dk>
2025-10-17 17:10:03 +0300

    delete experiment

--- examples/lumi_mixer/create_examples.sh
+++ examples/lumi_mixer/create_examples.sh
@@ -118,7 +118,6 @@ create_case ${experiment}  64  16  16 "${cluster}" 1
 create_case ${experiment} 128  16  16 "${cluster}" 1
 create_case ${experiment} 128  32  16 "${cluster}" 1
 create_case ${experiment} 128  32  32 "${cluster}" 1
-create_case ${experiment} 256  32  32 "${cluster}" 1 5
 
 # Investigation of the effect of memory checkpoints
 experiment="memory_checkpoints"
