e0459e4c8db24f31ad00e40d55b5e5db29d6b068
Tim Felle Olsen <tife@dtu.dk>
2026-01-07 09:47:23 +0100

    Add a max runtime to the optimizer (#336)
    Introduce a maximum runtime parameter for the optimizer, allowing it to
    terminate based on elapsed time. This change enhances control over
    optimization processes and includes updates to initialization and
    runtime procedures.

--- sources/optimizer/optimizer.f90
+++ sources/optimizer/optimizer.f90
@@ -1,6 +1,6 @@
 !> @file optimizer.f90
 !! @copyright
-!! Copyright (c) 2025, The Neko-TOP Authors
+!! Copyright (c) 2025-2026, The Neko-TOP Authors
 !! All rights reserved.
 !!
 !! Redistribution and use in source and binary forms, with or without
@@ -32,12 +32,11 @@
 !! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 !! POSSIBILITY OF SUCH DAMAGE.
 
+!> Defines the abstract type `optimizer`
+!! The optimizer type is defined to provide a generic interface to underlying
+!! optimization methods. Specific optimizers should extend this type and
+!! implement the deferred methods.
 module optimizer
-  !-----------------------------------------------------------!
-  ! An abstract type optimizer is defined to solve the design !
-  ! optimization problem using a specific type of optimizer   !
-  ! algorithm, e.g. MMA.                                      !
-  !-----------------------------------------------------------!
 
   use json_module, only: json_file
   use simulation_m, only: simulation_t
@@ -46,42 +45,59 @@ module optimizer
   use num_types, only: rp
   use logger, only: neko_log
   use profiler, only: profiler_start_region, profiler_end_region
+  use mpi_f08, only: MPI_Wtime
+  use utils, only: neko_error
 
   implicit none
   private
 
   !> Abstract optimizer class.
-  type, abstract :: optimizer_t
+  type, abstract, public :: optimizer_t
      private
 
      !> The maximum number of iterations
-     integer, public :: max_iterations
+     integer, public :: max_iterations = 0
      !> The tolerance for the optimization loop
-     real(kind=rp), public :: tolerance
+     real(kind=rp), public :: tolerance = 0.0_rp
+     !> Maximum runtime in seconds
+     real(kind=rp), public :: max_runtime = -1.0_rp
 
    contains
+
+     !  ---------------------------------------------------------------------- !
+     ! Deferred procedures for specific optimizers
+
      !> Initialize the optimizer, associate it with a specific problem
      procedure(optimizer_init_from_json), pass(this), public, deferred :: &
           init_from_json
-     !> Run the optimization loop
-     procedure, pass(this), public :: run => optimizer_run
-     !> Prepare the optimizer before starting the optimization loop
-     procedure(optimizer_initialize), pass(this), public, deferred :: &
-          initialize
-     !> Perform a single optimization step
-     procedure(optimizer_step), pass(this), public, deferred :: step
      !> Free resources.
      procedure(optimizer_free), pass(this), public, deferred :: free
 
+     !> Prepare the optimizer before starting the optimization loop
+     procedure(optimizer_initialize), pass(this), public, deferred :: initialize
+     !> Perform a single optimization step
+     procedure(optimizer_step), pass(this), public, deferred :: step
      !> Validate the solution
      procedure(optimizer_validate), pass(this), public, deferred :: validate
+
      !> Write the progress of the optimizer to the log file
      procedure(optimizer_write), pass(this), public, deferred :: write
+
+     ! ----------------------------------------------------------------------- !
+     ! Public procedures
+
+     !> Run the optimization loop
+     procedure, pass(this), public :: run => optimizer_run
+
+     ! ----------------------------------------------------------------------- !
+     ! Private procedures
 
      !> The base initializer
      procedure, pass(this) :: init_base => optimizer_init_base
      !> Free base resources.
      procedure, pass(this) :: free_base => optimizer_free_base
+     !> Print status message
+     procedure, pass(this) :: print_status => optimizer_print_status
 
   end type optimizer_t
 
@@ -164,7 +180,7 @@ module optimizer
      end subroutine optimizer_factory
   end interface optimizer_factory
 
-  public :: optimizer_t, optimizer_factory
+  public :: optimizer_factory
 
 contains
 
@@ -175,12 +191,15 @@ contains
   !! @param this The optimizer object.
   !! @param max_iterations The maximum number of iterations.
   !! @param tolerance The tolerance for the optimization loop.
-  subroutine optimizer_init_base(this, max_iterations, tolerance)
+  !! @param max_runtime The maximum runtime in seconds.
+  subroutine optimizer_init_base(this, max_iterations, tolerance, max_runtime)
     class(optimizer_t), intent(inout) :: this
     integer, intent(in) :: max_iterations
     real(kind=rp), intent(in) :: tolerance
+    real(kind=rp), intent(in), optional :: max_runtime
 
     this%max_iterations = max_iterations
+    this%max_runtime = max_runtime
     this%tolerance = tolerance
 
   end subroutine optimizer_init_base
@@ -189,6 +208,11 @@ contains
   !! @param this The optimizer object.
   subroutine optimizer_free_base(this)
     class(optimizer_t), intent(inout) :: this
+
+    this%max_iterations = 0
+    this%tolerance = 0.0_rp
+    this%max_runtime = -1.0_rp
+
   end subroutine optimizer_free_base
 
   ! -------------------------------------------------------------------------- !
@@ -197,6 +221,10 @@ contains
   !> Define the optimization loop
   !! This subroutine runs the optimization loop until convergence
   !! or the maximum number of iterations is reached.
+  !!
+  !! The optimization loop can be terminated based on a maximum runtime. In this
+  !! case, a cumulative average is used to determine if the next iteration would
+  !! exceed the maximum runtime.
   !!
   !! @param this The optimizer object.
   !! @param problem The problem object.
@@ -207,9 +235,11 @@ contains
     class(problem_t), intent(inout) :: problem
     class(design_t), intent(inout) :: design
     type(simulation_t), optional, intent(inout) :: simulation
+    real(kind=rp) :: start_time, elapsed_time
+    real(kind=rp) :: iteration_start_time, iteration_end_time
+    real(kind=rp) :: iteration_average_time
     logical :: converged
-    character(len=256) :: msg
-    integer :: iter
+    integer :: iter, stop_flag
 
     ! Prepare the problem state before starting the optimization
     call this%initialize(problem, design, simulation)
@@ -218,37 +248,87 @@ contains
 
     call neko_log%section('Optimization Loop')
 
-    iter = 1
+    stop_flag = 1
     converged = .false.
-    do while (iter .le. this%max_iterations .and. .not. converged)
+    start_time = MPI_Wtime()
+    iteration_average_time = 0.0_rp
+    do iter = 1, this%max_iterations
 
        call profiler_start_region('Optimizer iteration')
+       iteration_start_time = MPI_Wtime()
+
        converged = this%step(iter, problem, design, simulation)
+
+       iteration_end_time = MPI_Wtime()
        call profiler_end_region('Optimizer iteration')
 
        ! Log the progress and outputs
        call this%write(iter, problem)
        call design%write(iter)
 
-       iter = iter + 1
-    end do
+       ! --------------------------------------------------------------------- !
+       ! Check stopping criteria
 
-    call neko_log%end_section()
+       if (converged) then
+          stop_flag = 0
+          exit
+       else if (this%max_runtime .gt. 0.0_rp) then
+          elapsed_time = MPI_Wtime() - start_time
+
+          ! Estimate Cumulative Average iteration time
+          iteration_average_time = iteration_average_time * &
+               (real(iter, kind=rp) / real(iter + 1, kind=rp)) + &
+               (iteration_end_time - iteration_start_time) / &
+               real(iter + 1, kind=rp)
+
+          if (elapsed_time + iteration_average_time .gt. this%max_runtime) then
+             stop_flag = 2
+             exit
+          end if
+       end if
+    end do
 
     ! Check that the final design is valid
     call this%validate(problem, design)
+    call this%print_status(stop_flag, iter)
 
-    if (.not. converged) then
-       write(msg, '(A,I0,A)') 'Optimizer did not converge in ', &
-            this%max_iterations, ' iterations.'
-       call neko_log%warning(msg)
-    else
-       write(msg, '(A,I0,A)') 'Optimizer converged after ', iter, &
-            ' iterations.'
-       call neko_log%message(msg)
-    end if
+    call neko_log%end_section()
 
   end subroutine optimizer_run
 
+  !> Print status message
+  !! Supported flags:
+  !! 0: Converged successfully             (SUCCESS)
+  !! 1: Did not converge in max iterations (WARNING)
+  !! 2: Stopped after reaching max runtime (ERROR)
+  !!
+  !! @param this The optimizer object.
+  !! @param stop_flag The stopping flag.
+  !! @param iter The number of iterations performed.
+  subroutine optimizer_print_status(this, stop_flag, iter)
+    class(optimizer_t), intent(in) :: this
+    integer, intent(in) :: stop_flag
+    integer, intent(in) :: iter
+    character(len=256) :: msg
+
+    select case (stop_flag)
+    case (0)
+       write(msg, '(A,I0,A)') 'Optimizer converged successfully after ', &
+            iter, ' iterations.'
+       call neko_log%message(msg)
+    case (1)
+       write(msg, '(A,I0,A)') 'Optimizer did not converge in ', &
+            this%max_iterations, ' iterations.'
+       call neko_log%warning(msg)
+    case (2)
+       write(msg, '(A,A,F8.2,A)') 'Optimizer stopped after reaching the ', &
+            'maximum runtime of ', this%max_runtime, ' seconds.'
+       call neko_error(trim(msg))
+
+    case default
+       write(msg, '(A)') 'Optimizer stopped for an unknown reason.'
+       call neko_error(msg)
+    end select
+  end subroutine optimizer_print_status
 
 end module optimizer
