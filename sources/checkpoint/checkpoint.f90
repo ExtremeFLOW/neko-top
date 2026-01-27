43f0e11b28247fa8877171ee24f50ec1ceeb71a9
gloopydoop <nobis@mech.kth.se>
2026-01-27 10:23:49 +0100

    cleaning

--- sources/checkpoint/checkpoint.f90
+++ sources/checkpoint/checkpoint.f90
@@ -100,8 +100,6 @@ module simulation_checkpoint
      procedure, public, pass(this) :: save => checkpoint_save
      !> Restore the forward simulation state
      procedure, public, pass(this) :: restore => checkpoint_restore
-     !> Public getter to check if the simulation is unsteady
-     procedure, public, pass(this) :: get_enabled => checkpoint_get_enabled
 
   end type simulation_checkpoint_t
 
@@ -325,15 +323,6 @@ contains
 
     call profiler_end_region("Checkpoint restore")
   end subroutine checkpoint_restore
-
-  !> Restore the forward simulation state
-  subroutine checkpoint_get_enabled(this, enabled)
-    class(simulation_checkpoint_t), intent(inout) :: this
-    logical, intent(out) :: enabled
-
-    enabled = this%enabled
-
-  end subroutine checkpoint_get_enabled
 
   ! ========================================================================== !
   ! Meta handling
