a5643806e17a9220436cbaa410181e22cc5483f0
Tim Felle Olsen <tife@dtu.dk>
2026-05-29 10:16:30 +0200

    Ensure rounding is done correctly.

--- sources/checkpoint/checkpoint_linear.f90
+++ sources/checkpoint/checkpoint_linear.f90
@@ -76,8 +76,11 @@ contains
     ! Only save to RAM from the last disc checkpoint to the end of the forward
     ! simulation. With fixed timesteps, the total count and the last disc-save
     ! timestep are known from the time object.
-    n_total = nint((neko_case%time%end_time - neko_case%time%start_time) &
-         / neko_case%time%dt)
+    ! Note: the plus 0.5 is to round up to the next integer, as the division can
+    ! be slightly smaller than the actual number of steps due to floating point
+    ! errors.
+    n_total = int(((neko_case%time%end_time - neko_case%time%start_time) &
+         / neko_case%time%dt) + 0.5_rp)
     if (tstep .ge. n_total - modulo(n_total, this%n_saves_memory)) then
        call this%save_data(index + 1)
     end if
