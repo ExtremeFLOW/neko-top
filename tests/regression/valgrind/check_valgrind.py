0d7018f9696e05c4e812f04579efc38a5819e5d8
Tim Felle Olsen <tife@dtu.dk>
2026-06-10 14:39:20 +0200

    Default to 0

--- tests/regression/valgrind/check_valgrind.py
+++ tests/regression/valgrind/check_valgrind.py
@@ -67,7 +67,7 @@ def main() -> int:
     parser.add_argument(
         "--max-reachable-bytes",
         type=int,
-        default=460000,
+        default=0,
         help="Maximum allowed still-reachable bytes",
     )
     args = parser.parse_args()
