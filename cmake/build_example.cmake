9c9a6bae3a8d0f6eb82cbd311d3079d98b4dc8e8
Harrison Nobis <58177854+gloopydoop@users.noreply.github.com>
2026-08-30 10:36:00 +0200

    Update cmake/build_example.cmake
    Co-authored-by: Tim Felle Olsen <timfelle@hotmail.com>

--- cmake/build_example.cmake
+++ cmake/build_example.cmake
@@ -118,7 +118,6 @@ function(build_example)
         PkgConfig::neko
         PkgConfig::json-fortran
         MPI::MPI_Fortran
-        MPI::MPI_CXX
         $<$<BOOL:${BLAS_FOUND}>:BLAS::BLAS>
         $<$<BOOL:${LAPACK_FOUND}>:LAPACK::LAPACK>
         neko_cxx_support
