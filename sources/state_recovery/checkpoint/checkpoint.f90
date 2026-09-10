0358522275ec449f85f5454484af0386f85d6f19
Tim Felle Olsen <timfelle@hotmail.com>
2026-09-10 07:45:42 +0200

    Apply suggestion from @timfelle

--- sources/state_recovery/checkpoint/checkpoint.f90
+++ sources/state_recovery/checkpoint/checkpoint.f90
@@ -519,8 +519,6 @@ contains
                   this%state_storage(i, j)%size)
           else
              call neko_error("State storage not allocated")
-          else
-             call neko_error("State storage not allocated")
           end if
        end do
     end do
