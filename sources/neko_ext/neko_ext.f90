864f0bc4ca4c1b963106a0e47f35d7e17de35208
Tim Felle Olsen <tife@dtu.dk>
2026-09-24 12:52:00 +0000

    Fix/AB reset (#520)

--- sources/neko_ext/neko_ext.f90
+++ sources/neko_ext/neko_ext.f90
@@ -62,6 +62,7 @@ module neko_ext
   use field_math, only: field_rzero, field_copy
   use fluid_pnpn, only: fluid_pnpn_t
   use adjoint_fluid_pnpn, only: adjoint_fluid_pnpn_t
+  use scalar_pnpn, only: scalar_pnpn_t
 
   implicit none
 
@@ -168,6 +169,12 @@ contains
     call json_get(neko_case%params, 'case.fluid.initial_condition', &
          json_subdict)
 
+    ! Reset the fields. The ICs often assumes these are 0.
+    call field_rzero(p)
+    call field_rzero(u)
+    call field_rzero(v)
+    call field_rzero(w)
+
     if (trim(string_val) .ne. 'user') then
        call set_flow_ic(u, v, w, p, &
             neko_case%fluid%c_Xh, neko_case%fluid%gs_Xh, &
@@ -213,6 +220,13 @@ contains
        end if
        ! zero out RHS
        call field_rzero(neko_case%scalars%scalar_fields(1)%scalar%f_Xh)
+       ! zero out the Adams-Bashforth history, mirroring the fluid above.
+       select type (s_scheme => &
+            neko_case%scalars%scalar_fields(1)%scalar)
+       type is (scalar_pnpn_t)
+          call field_rzero(s_scheme%abx1)
+          call field_rzero(s_scheme%abx2)
+       end select
        ! reset the forward scalar
        call json_get(neko_case%params, &
             'case.scalar.initial_condition.type', string_val)
