8bbf3d850050d5e04f0f8efcbe6f292874c1fe1d
Tim Felle Olsen <tife@dtu.dk>
2026-09-24 08:35:14 +0200

    Realign source term initialization with Neko PR #1947 (#219)
    Update the initialization subroutines for various source terms to
    include a new parameter, `variable_name`, aligning with changes made in
    Neko PR #1947. This enhances the flexibility of the initialization
    process.

--- sources/source_terms/simple_brinkman_source_term.f90
+++ sources/source_terms/simple_brinkman_source_term.f90
@@ -77,12 +77,14 @@ contains
   !! @param json The JSON object for the source.
   !! @param fields A list of fields for adding the source values.
   !! @param coef The SEM coeffs.
+  !! @param variable_name The name of the variable where the source term acts.
   subroutine simple_brinkman_source_term_init_from_json(this, json, fields, &
-       coef)
+       coef, variable_name)
     class(simple_brinkman_source_term_t), intent(inout) :: this
     type(json_file), intent(inout) :: json
     type(field_list_t), intent(in), target :: fields
     type(coef_t), intent(in), target :: coef
+    character(len=*), intent(in) :: variable_name
 
 
     ! we shouldn't be initializing this from JSON
