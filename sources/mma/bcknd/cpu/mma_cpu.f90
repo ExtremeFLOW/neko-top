02094171a95affe8c4a2f429aba683fb8c4d6bb6
Tim Felle Olsen <tife@dtu.dk>
2026-09-26 07:40:39 +0200

    Update sources/mma/bcknd/cpu/mma_cpu.f90

--- sources/mma/bcknd/cpu/mma_cpu.f90
+++ sources/mma/bcknd/cpu/mma_cpu.f90
@@ -267,8 +267,8 @@ contains
 
       if (iter .lt. 3) then
          ! Initialize the lower and upper asymptotes
-         low = x - this%asyinit * xmax_diff
-         upp = x + this%asyinit * xmax_diff
+         low = x - this%asyinit * x_diff
+         upp = x + this%asyinit * x_diff
       else
          do j = 1, this%n
             if ((x(j) - x_1(j)) * (x_1(j) - x_2(j)) .lt. 0.0_rp) then
